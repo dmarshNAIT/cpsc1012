aaf27bf0a00b500c5cbf3302ff9d79dd7fd3c09c
dmarshNAIT <55507008+dmarshNAIT@users.noreply.github.com>
2019-10-02 11:13:41 -0600

    Create whileLoop-A02.cs

---
+++ whileLoop-A02.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Oct1Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            int num, numFactorial, count;
+
+            // prompt user
+            Console.WriteLine("give me a number:");
+            num = int.Parse(Console.ReadLine());
+
+            // declare initial values for loop
+            count = num;
+
+            // run our loop until we get to 1
+            while (count > 1)
+            {
+                // multiply count * (count -1 )
+                // decrement 
+
+            }
+
+            // display results
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
