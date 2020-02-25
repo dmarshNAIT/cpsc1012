1d5a807a5ce7c43e33452789e7a3bbd372418715
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2020-02-25 16:46:11 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace A04_Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            bool validInput = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter your age: ");
+                    age = int.Parse(Console.ReadLine());
+                    validInput = true;
+                    Console.WriteLine($"Your age is {age}.");
+                } // end try
+                catch
+                {
+                    Console.WriteLine("Sorry, invalid.");
+                } // end catch
+
+            } while (!validInput);
+
+            Console.WriteLine("We got to the end!");
+
+        } // end method
+    } // end class
+} // end namespace
