269db305ce270a5b97df82ced0f55a2333bced83
dmarshNAIT <55507008+dmarshNAIT@users.noreply.github.com>
2020-03-12 17:08:11 -0600

    Posted ConnectFour

---
+++ Connect4/Connect4/ConnectFour.cs
@@ -0,0 +1,236 @@
+﻿/* Class to play Connect 4.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Connect4
+{
+    class ConnectFour
+    {
+        private char[,] gameBoard;
+        private char humanSymbol;
+        private char computerSymbol;
+        private ConsoleColor humanColour;
+        private ConsoleColor computerColour = ConsoleColor.Green;
+        private bool humanTurn = true;
+        private bool gameOver = false;
+        private int numRows = 6; // standard board size
+        private int numColumns = 7;  // standard board size
+        private int spacesRemaining;
+
+        public ConnectFour()
+        {
+            gameBoard = new char[numRows, numColumns];
+            Console.Clear();
+
+            // set symbols and colours for each player:
+            humanSymbol = Program.GetUserChar("What symbol would you like to play with?");
+            computerSymbol = (humanSymbol == '*') ? '@' : '*'; // make sure the computer uses a different symbol
+            humanColour = Program.GetUserColour();
+            spacesRemaining = numColumns * numRows;
+
+            Console.WriteLine("\nPlease wait while we set up your board...");
+            Thread.Sleep(2000); // dramatic pause
+
+        } // end method
+
+        public void DisplayBoard()
+        {
+            Console.Clear();
+            string horizontalLine = "\n" + new string('-', this.gameBoard.GetLength(1) * 4 + 1);
+
+            Console.Write("  ");
+            for (int columnIndex = 0; columnIndex < this.gameBoard.GetLength(1); columnIndex++)
+            {
+                Console.Write(columnIndex);
+                Console.Write("   ");
+            } // end for
+
+            Console.WriteLine(horizontalLine);
+
+            for (int rowIndex = 0; rowIndex < this.gameBoard.GetLength(0); rowIndex++)
+            {
+                Console.Write("| ");
+                for (int columnIndex = 0; columnIndex < this.gameBoard.GetLength(1); columnIndex++)
+                {
+                    string symbolToPrint = Char.ToString(this.gameBoard[rowIndex, columnIndex]);
+                    if (this.gameBoard[rowIndex, columnIndex] == humanSymbol)
+                    {
+                        Program.WriteInColour(symbolToPrint, humanColour);
+                    } // end if
+                    else if (this.gameBoard[rowIndex, columnIndex] == computerSymbol)
+                    {
+                        Program.WriteInColour(symbolToPrint, computerColour);
+                    } // end if
+                    else
+                    {
+                        Console.Write(symbolToPrint);
+                    } // end else         
+                    Console.Write(" | ");
+                } // end inner for
+                Console.WriteLine(horizontalLine);
+            } // end for
+        } // end method
+
+        public int GetIndexOfLowestAvailableSpot(char[,] board, int columnToCheck)
+        {
+            // check each row in the column
+            for (int rowIndex = board.GetLength(0) - 1; rowIndex >= 0; rowIndex--)
+            {
+                if (board[rowIndex, columnToCheck] == '\0')
+                    return rowIndex;
+            } // end for
+
+            return -1; // return -1 if there is no room left
+
+        } // end method
+
+        public void PlayGame()
+        {
+            do
+            {
+                DisplayBoard();
+
+                PlayHumanTurn();
+
+                if (!gameOver)
+                {
+                    Console.WriteLine("Now it's the computer's turn!");
+                    Thread.Sleep(1000);
+
+                    PlayComputerTurn();
+
+                    if (spacesRemaining == 0) // also check after human's turn if it's not an even number of squares
+                    {
+                        gameOver = true;
+                        Console.WriteLine("It's a tie game!");
+                    } // end if
+
+                } // end if
+
+            } while (!gameOver);
+
+        } // end method
+
+        public void PlayHumanTurn()
+        {
+            // human turn:
+            int whichColumn = Program.GetUserInt("Where would you like to play?");
+
+            // check if it's a valid column
+            while (whichColumn < 0 || whichColumn >= numColumns)
+            {
+                whichColumn = Program.GetUserInt("That's not a valid column. Please try another move.");
+            } // end while
+
+            // see if that column has room
+            int rowToPlay = GetIndexOfLowestAvailableSpot(gameBoard, whichColumn);
+            while (rowToPlay == -1)
+            {
+                whichColumn = Program.GetUserInt("That column is full. Please try another move.");
+
+                // check if it's a valid column
+                while (whichColumn < 0 || whichColumn >= numColumns)
+                {
+                    whichColumn = Program.GetUserInt("That's not a valid column. Please try another move."); // repetitive -- come back to this later
+                } // end while
+
+                rowToPlay = GetIndexOfLowestAvailableSpot(gameBoard, whichColumn);
+            } // end while
+
+            // if so, place, showing animation:
+            PlayMove(whichColumn, rowToPlay);
+
+            // check if win
+            if (CheckIfWinner())
+            {
+                gameOver = true;
+                Console.WriteLine("You win!");
+            } // end if
+        } // end method
+
+        public void PlayComputerTurn()
+        {
+            // randomly generate a column
+            Random numGenerator = new Random();
+            int whichColumn = numGenerator.Next(numColumns);
+
+            // make sure it's available
+            int rowToPlay = GetIndexOfLowestAvailableSpot(gameBoard, whichColumn);
+            while (rowToPlay == -1)
+            {
+                whichColumn = numGenerator.Next(numColumns);
+                rowToPlay = GetIndexOfLowestAvailableSpot(gameBoard, whichColumn);
+            } // end while
+
+            PlayMove(whichColumn, rowToPlay);
+
+            // check if win
+            if (CheckIfWinner())
+            {
+                gameOver = true;
+                Console.WriteLine("Computer wins!");
+            } // end if
+
+        } // end method
+
+        public void PlayMove(int whichColumn, int rowToPlay)
+        {
+            DisplayBoard();
+
+            // for each empty spot in the column:
+            for (int rowIndex = 0; rowIndex < rowToPlay; rowIndex++)
+            {
+                gameBoard[rowIndex, whichColumn] = (humanTurn) ? humanSymbol : computerSymbol; // add the symbol to the top empty spot
+                DisplayBoard();
+                Thread.Sleep(500); // dramatic pause
+                gameBoard[rowIndex, whichColumn] = '\0'; ; // remove the symbol
+            } // end for
+
+            gameBoard[rowToPlay, whichColumn] = (humanTurn) ? humanSymbol : computerSymbol; // play final turn
+            spacesRemaining--;
+            humanTurn = !humanTurn; // switch to other player's turn
+            DisplayBoard();
+
+        } // end method
+
+        public bool CheckIfWinner()
+        {
+            // ways to win:
+            // 4 in a row
+            for (int rowIndex = 0; rowIndex <= numRows - 1; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex <= numColumns - 4; columnIndex++)
+                {
+                    if (gameBoard[rowIndex, columnIndex] != '\0' &&
+                        gameBoard[rowIndex, columnIndex] == gameBoard[rowIndex, columnIndex + 1] &&
+                        gameBoard[rowIndex, columnIndex + 1] == gameBoard[rowIndex, columnIndex + 2] &&
+                        gameBoard[rowIndex, columnIndex + 2] == gameBoard[rowIndex, columnIndex + 3])
+                        return true;
+                } // end inner for
+
+            } // end for
+
+            // 4 in a column
+            for (int rowIndex = 0; rowIndex <= numRows - 4; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex <= numColumns - 1; columnIndex++)
+                {
+                    if (gameBoard[rowIndex, columnIndex] != '\0' &&
+                        gameBoard[rowIndex, columnIndex] == gameBoard[rowIndex + 1, columnIndex] &&
+                        gameBoard[rowIndex + 1, columnIndex] == gameBoard[rowIndex + 2, columnIndex] &&
+                        gameBoard[rowIndex + 2, columnIndex] == gameBoard[rowIndex + 3, columnIndex])
+                        return true;
+                } // end inner for
+
+            } // end for
+
+            return false;
+        } // end method
+
+    } // end class
+} // end namespace
---
+++ Connect4/Connect4/Program.cs
@@ -0,0 +1,94 @@
+﻿/* Game to play Connect 4, using the ConnectFour class.
+ * This was written in one sitting and only briefly tested, so please let me know if you find any bugs!
+ * Thanks, and enjoy!
+ */
+
+
+using System;
+
+namespace Connect4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char playAgain;
+            do
+            {
+                // generate a board:
+                ConnectFour game = new ConnectFour();
+                game.PlayGame();
+
+                playAgain = GetUserChar("Want to play another round? (y/n)");
+
+            } while (char.ToLower(playAgain) == 'y');
+
+            Console.Write("Thanks for playing!");
+            Console.ReadLine();
+
+        } // end method
+
+        public static char GetUserChar(string prompt)
+        {
+            char userInput;
+            Console.WriteLine(prompt);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            while (!char.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.ResetColor();
+                Console.WriteLine($"Sorry, invalid. Please try again.\n{prompt}");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            } // end while
+            Console.ResetColor();
+            return userInput;
+        } // end method
+
+        public static int GetUserInt(string prompt)
+        {
+            int userInput;
+            Console.WriteLine(prompt);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            while (!int.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.ResetColor();
+                Console.WriteLine($"Sorry, invalid. Please try again.\n{prompt}");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            } // end while
+            Console.ResetColor();
+            return userInput;
+        } // end method
+
+        public static ConsoleColor GetUserColour()
+        {
+            ConsoleColor chosenColor;
+            char userResponse = GetUserChar("Would you like to play with [R]ed, [B]lue, or [C]yan?");
+            switch (char.ToUpper(userResponse))
+            {
+                case 'R':
+                    chosenColor = ConsoleColor.Red;
+                    break;
+                case 'B':
+                    chosenColor = ConsoleColor.Blue;
+                    break;
+                case 'C':
+                    chosenColor = ConsoleColor.Cyan;
+                    break;
+                default:
+                    Console.WriteLine("That wasn't an option. I'll pick for you.");
+                    chosenColor = ConsoleColor.Magenta;
+                    break;
+            } // end switch
+            return chosenColor;
+        } // end method
+
+        public static void WriteInColour(string message, ConsoleColor colour, bool writeLine = false)
+        {
+            Console.ForegroundColor = colour;
+            Console.Write(message);
+            Console.ResetColor();
+            if (writeLine)
+                Console.WriteLine();
+        } // end method
+
+    } // end program
+} // end class
