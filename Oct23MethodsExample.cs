0d2f7f8ac04878bfc1eb287d41fae90d18d30f01
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2020-10-23 18:47:11 -0600

    Update Oct23MethodsExample.cs

--- Oct23MethodsExample.cs
+++ Oct23MethodsExample.cs
@@ -14,24 +14,22 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
+            int menuSelection;
             do
             {
-                // DisplayMenu();
-                    // 1. Play Rock Paper Scissors
-                    // 2. Play NumberGuesser5000
-                    // 3. Get the current time
-                    // 0. Exit
-                int userChoice = GetUserChoice();
-                switch (userChoice)
+                DisplayMenu();
+                menuSelection = GetInputInt("What is your choice?");
+                switch (menuSelection)
                 {
                     case 1:
-                        // PlayRockPaperScissors();
+                        PlayRockPaperScissors();
                         break;
                     case 2:
                         // PlayNumberGuesser();
                         break;
                     case 3:
-                        // GetCurrentTime();
+                        // GetCurrentTime(); 
+                        // using DateTime.Now.ToString()
                         break;
                     case 0:
                         break;
@@ -40,12 +38,50 @@ namespace Sandbox
                         break;
                 }
 
-            } while (userInput != 0);
+            } while (menuSelection != 0);
 
         } // end of method
-        static string GetInputString()
+
+        static void DisplayMenu()
         {
-            Console.Write("Enter your name: ");
+            Console.WriteLine("1.Play Rock Paper Scissors\n" +
+             "2.Play NumberGuesser5000\n" +
+             "3.Get the current time\n" +
+             "0.Exit\n");
+
+        } // end of DisplayMenu()
+
+        static void PlayRockPaperScissors()
+        {
+            // ask the user what move they are making
+            int userMove = GetInputInt("Are you (1) rock, (2) paper, or (3) scissors?");
+
+            // randomly generate computer move
+            Random numGenerator = new Random();
+            int computerMove = numGenerator.Next(1, 4);
+
+            // compare
+            if (userMove == computerMove)
+                Console.WriteLine("It's a tie!");
+            else if (userMove == 1 && computerMove == 3 || // rock beats scissors
+                    userMove == 3 && computerMove == 2 || // scissors beats paper
+                    userMove == 2 && computerMove == 1) // paper beats rock
+
+                // userMove is 1 larger than computerMove % 3 = 0 
+                // userMove is 1 larger than computerMove % 3 = 2
+                // userMove is 1 larger than computerMove % 3 = 1
+
+                // else if (userMove == (computerMove % 3) + 1)
+
+                Console.WriteLine("You won!");
+            else
+                Console.WriteLine("You lost.");
+
+        }
+
+        static string GetInputString(string prompt)
+        {
+            Console.Write(prompt);
             string name = Console.ReadLine();
             return name;
         } // end of GetInput()
