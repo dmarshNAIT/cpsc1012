af28ece182a0b5431ae9f4a6a84c12ef10139780
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-09-08 16:54:14 -0600

    Added Sep 8 class examples

--- Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
+++ Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
@@ -5,7 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!\nHi I'm Dana.");
-            Console.WriteLine();
+
+
+            // Display welcome message
+            Console.WriteLine("Welcome to our super awesome calculator.");
+
+            // Prompt user for value 1
+            // we don't know how to get input yet, so let's hardcode some values for now.
+
+            int value1;
+            value1 = 2;
+            Console.WriteLine("The first number is " + value1);
+
+            int value2;
+
+
+            // Prompt user for value 2
+            // Calculate value1 + value 2
+            // Calculate value1 - value 2
+            // Calculate value1 * value 2
+            // Calculate value1 / value 2
+            // Display value1 + value 2
+            // Display value1 / value 2
+            // Display value1 - value 2
+            // Display value1 * value 2
+
+
+
+
         }
     }
 }
