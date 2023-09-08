6a34dc2af55538344110e8767043441fecf4c9cb
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-09-08 12:49:33 -0600

    Added class examples

---
+++ Fall 2023 - Section 4/SandboxA04/SandboxA04/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SandboxA04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! \nHi I'm still Dana");
+
+            // Display welcome to user
+            Console.WriteLine("Welcome to our calculator! Math is awesome.");
+
+            // create variables to hold our user's input
+            int value1;
+            int value2;
+
+            // Prompt user for value 1
+            value1 = 2; 
+            // we haven't learned how to read input from the user yet :(
+            // so this will be our temporary workaround.
+
+            // Prompt user for value 2
+            value2 = 3;
+            Console.WriteLine(value2);
+
+            // Calculate sum
+            // Display sum
+            // Calculate difference
+            // Display difference
+            // Calculate product
+            // Display product
+            // Calculate quotient
+            // Display quotient
+
+        }
+    }
+}
---
+++ Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SandboxA05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!\nHi I'm Dana.");
+            Console.WriteLine();
+        }
+    }
+}
