33c64470b07e46f1c6a5193b3c327d0f19b361f2
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-09-11 16:49:39 -0600

    Sep 11 class examples

--- Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
+++ Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
@@ -3,9 +3,8 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Hello, World!\nHi I'm Dana.");
-
 
             // Display welcome message
             Console.WriteLine("Welcome to our super awesome calculator.");
@@ -17,22 +16,36 @@
             value1 = 2;
             Console.WriteLine("The first number is " + value1);
 
-            int value2;
-
-
             // Prompt user for value 2
+            int value2;
+            value2 = 3;
+
             // Calculate value1 + value 2
-            // Calculate value1 - value 2
-            // Calculate value1 * value 2
-            // Calculate value1 / value 2
+            int sum;
+            sum = value1 + value2;
+
             // Display value1 + value 2
+            Console.WriteLine("The sum of " + value1 + " and " + value2 + " is " + sum);
+
+
+            // Calculate value1 - value 2
+            int diff = value1 - value2;
+
+
             // Display value1 / value 2
-            // Display value1 - value 2
+            Console.WriteLine("The difference between {0} and {1} is {2}.", value1, value2, diff);
+
+            // Calculate value1 * value 2
             // Display value1 * value 2
 
 
+            // Calculate value1 / value 2
+            double quotient = (double) value1 / value2;
+            Console.WriteLine("The answer is " + quotient);
 
+            // Display value1 / value 2
 
         }
     }
 }
+     
