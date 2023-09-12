7bedb21876c8e32a4e3950230d21929038f6093f
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-09-12 07:59:43 -0600

    Updated sandbox

---
+++ Fall 2023 - Evening Section 2/SandboxOE02/SandboxOE02/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SandboxOE02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("hi I'm Dana. Welcome to my awesome calculator.");
+
+            //	Read num1
+            // we don't know how to read input from the user, so let's hardcode some values
+            int num1;
+            num1 = 2;
+            Console.WriteLine("The first number is: " + num1);
+
+            //	Read num2
+            int num2 = 3;
+            Console.WriteLine("The 2nd number is: " + num2);
+
+            //	Calculate sum = num1 + num2
+            int sum = num1 + num2;
+
+            //	Display sum
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+            //	Calculate difference = num1 – num2
+            //	Display difference
+            //	Calculate product = num1 * num2
+            //	Display product
+
+            //***** STOP HERE *****//
+            //	Calculate quotient = num1 / num2
+            //	Display quotient
+
+
+            // TO DO: What happens if they divide by zero??
+
+            Console.ReadLine(); // waiting for user input
+        }
+    }
+}
--- Fall 2023 - Section 4/SandboxA04/SandboxA04/Program.cs
+++ Fall 2023 - Section 4/SandboxA04/SandboxA04/Program.cs
@@ -7,28 +7,28 @@
             Console.WriteLine("Hello, World! \nHi I'm still Dana");
 
             // Display welcome to user
-            Console.WriteLine("Welcome to our calculator! Math is awesome.");
+            Console.WriteLine("Welcome to our calculator! Math is awesome.\n");
 
             // create variables to hold our user's input
             int value1;
             int value2;
 
             // Prompt user for value 1
-            value1 = 2; 
+            value1 = 2;
             // we haven't learned how to read input from the user yet :(
             // so this will be our temporary workaround.
+            Console.WriteLine("The 1st number is: " + value1);
 
             // Prompt user for value 2
             value2 = 3;
-            Console.WriteLine(value2);
+            Console.WriteLine("The 2nd number is: " + value2);
 
             // Calculate sum
             int sum;
             sum = value1 + value2;
 
             // Display sum
-            Console.WriteLine("The sum of " + value1 + " and " + value2 + " is " + sum);
-
+            Console.WriteLine("The sum of " + value1 + " and " + value2 + " is " + sum + ".");
 
             // Calculate difference
             int diff = value1 - value2;
@@ -36,14 +36,15 @@
             // Display difference
             Console.WriteLine("The difference between {0} and {1} is {2}.", value1, value2, diff);
 
-
             // Calculate product
             // Display product
+            Console.WriteLine("The product of {0} and {1} is {2}.", value1, value2, value1*value2);
+
             // Calculate quotient
             double quotient = (double) value1 / value2;
-            Console.WriteLine("Quotient is " + quotient);
 
             // Display quotient
+            Console.WriteLine("Quotient is " + quotient);
 
         }
     }
