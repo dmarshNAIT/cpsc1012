a8bcfdd04b2c78030de811d436a1b2ec08e810d9
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-09-13 22:06:18 -0600

    sep 13 class examples

--- Fall 2023 - Evening Section 2/SandboxOE02/SandboxOE02/Program.cs
+++ Fall 2023 - Evening Section 2/SandboxOE02/SandboxOE02/Program.cs
@@ -6,37 +6,79 @@
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("hi I'm Dana. Welcome to my awesome calculator.");
+            Console.WriteLine("hi I'm Dana. \nWelcome to my awesome calculator.\n\n");
+
+            Console.WriteLine("What is your name?");
+            string userInput;
+            Console.ForegroundColor = ConsoleColor.Green; // bonus content: this will make our user input green
+            userInput = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White; // and now we'll switch back to white text
+
+            Console.WriteLine($"Nice to meet you, {userInput}.\n");
 
             //	Read num1
-            // we don't know how to read input from the user, so let's hardcode some values for now
+            Console.Write("Please enter 1st number: ");
+            userInput = Console.ReadLine(); // this reads the input from the user
             int num1; // this line declares (or creates) the variable
-            num1 = 2; // this line assigns that variable a value
+            num1 = int.Parse(userInput); // this line assigns that variable a value
             Console.WriteLine("The first number is: " + num1);
 
             //	Read num2
-            int num2 = 3; // this statement does the declaration & initialization in one step!
+            Console.Write("Please enter 2nd number: ");
+            int num2 = int.Parse(Console.ReadLine()); // this is a little more advanced: we are combining 3 statements in one.
             Console.WriteLine("The 2nd number is: " + num2);
 
             //	Calculate sum = num1 + num2
             int sum = num1 + num2;
-
             //	Display sum
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is: " + sum);
 
             //	Calculate difference = num1 – num2
+            int diff = num1 - num2;
             //	Display difference
+            Console.WriteLine("The difference between {0} and {1} is: {2}", num1, num2, diff);
+
             //	Calculate product = num1 * num2
             //	Display product
+            Console.WriteLine($"The product of {num1} and {num2} is: {num1 * num2}");
 
-            //***** STOP HERE *****//
             //	Calculate quotient = num1 / num2
+            double quotient = (double)num1 / num2;
             //	Display quotient
+            Console.WriteLine($"The quotient of {num1} and {num2} is: {quotient}\n\n\n");
 
 
-            // TO DO: What happens if they divide by zero??
 
-            Console.ReadLine(); // waiting for user input
+            /* let's play around with data types */
+            int myInt = 456;
+            double myDouble = myInt; // implicit conversion
+            Console.WriteLine("myInt: " + myInt);
+            Console.WriteLine("myDouble: " + myDouble);
+            // this worked: doubles are able to hold the values of ints
+
+            myDouble = 789;
+            myInt = (int) myDouble; //explicit conversion
+            Console.WriteLine("myDouble: " + myDouble);
+            Console.WriteLine("myInt: " + myInt);
+            // this also works.
+
+            myDouble = 5000000000;
+            myInt = (int)myDouble; //explicit conversion
+            Console.WriteLine("myDouble: " + myDouble);
+            Console.WriteLine("myInt: " + myInt);
+            // this doesn't really work: 5billion is too big to be an int.
+
+            myDouble = 3.99999999;
+            myInt = (int)myDouble; //explicit conversion
+            Console.WriteLine("myDouble: " + myDouble);
+            Console.WriteLine("myInt: " + myInt);
+            // this also doesn't really work: we've lost all the decimals and have a misleading number left over.
+
+
+
+
+            // TO DO in the future (we need more skills first): What happens if they divide by zero??
+
         }
     }
 }
--- Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
+++ Fall 2023 - Section 5/SandboxA05/SandboxA05/Program.cs
@@ -3,22 +3,25 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Hello, World!\nHi I'm Dana.");
+        {
+            Console.WriteLine("Hello, World!\nHi I'm Dana. What's your name?");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Nice to meet you, {0}!\n", userInput);
 
             // Display welcome message
             Console.WriteLine("Welcome to our super awesome calculator.");
 
             // Prompt user for value 1
-            // we don't know how to get input yet, so let's hardcode some values for now.
-
-            int value1;
-            value1 = 2;
-            Console.WriteLine("The first number is " + value1);
+            Console.Write("Please enter 1st number: ");
+            userInput = Console.ReadLine(); // this reads input from the user
+            int value1 = int.Parse(userInput); // this turns it into an int, if possible
+            Console.WriteLine("You entered: " + value1);
 
             // Prompt user for value 2
-            int value2;
-            value2 = 3;
+            Console.Write("Please enter 2nd number: ");
+            userInput = Console.ReadLine(); // this reads input from the user
+            int value2 = int.Parse(userInput); // this turns it into an int, if possible
+            Console.WriteLine("You entered: " + value2 + "\n");
 
             // Calculate value1 + value 2
             int sum;
@@ -38,12 +41,37 @@
             Console.WriteLine("The product of {0} and {1} is {2}.", value1, value2, value1 * value2);
 
             // Calculate value1 / value 2
-            double quotient = (double) value1 / value2;
+            double quotient = (double)value1 / value2;
             // Display value1 / value 2
-            Console.WriteLine("The answer is " + quotient);
+            Console.WriteLine("The quotient is " + quotient);
 
 
+
+            /* Let's play around with data types */
+            int myInt = 6;
+            double myDouble = myInt; // implicit conversion
+            Console.WriteLine("\n\nmyInt is " + myInt);
+            Console.WriteLine("myDouble is " + myDouble);
+            // this works: ints can be cast to doubles
+
+            myDouble = 27;
+            myInt = (int)myDouble; // explicit conversion
+            Console.WriteLine("myDouble is " + myDouble);
+            Console.WriteLine("myInt is " + myInt);
+            // this also worked: 27 is a valid value for an int
+
+            myDouble = 5000000000;
+            myInt = (int)myDouble; // explicit conversion
+            Console.WriteLine("myDouble is " + myDouble);
+            Console.WriteLine("myInt is " + myInt);
+            // 5billion is "too big" to be saved as an int so we see a different result
+
+            myDouble = 3.999999999;
+            myInt = (int)myDouble; // explicit conversion
+            Console.WriteLine("myDouble is " + myDouble);
+            Console.WriteLine("myInt is " + myInt);
+            // ints cannot hold decimals so they are cut off
         }
+
     }
 }
-     
