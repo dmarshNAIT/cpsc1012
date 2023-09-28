6c5d1fc1019de0b6af146fa43c5337a06f9b9be7
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-09-27 20:03:02 -0600

    Sep 27 class examples

---
+++ Fall 2023 - Evening Section 2/SandboxOE02/Sep27DecisionStructures/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Sep27DecisionStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            string userInput;
+            int mark;
+            char letterGrade;
+
+            // prompt the user to enter their mark (as a whole #)
+            Console.Write("Please enter your mark (as a whole #): ");
+            userInput = Console.ReadLine();
+            mark = int.Parse(userInput); // is a little faster
+            mark = Convert.ToInt32(userInput); // is better at dealing with blank strings
+
+            // if their mark is above 80, congrats!
+            if (mark > 80)
+            {
+                Console.WriteLine("Congratulations!");
+            }
+            // if their mark is below 50:
+            else if (mark < 50)
+            {
+                Console.WriteLine("Maybe you would like to schedule an office hour."); ;
+            }
+            // otherwise, tell them to keep up the good work.
+            else
+            {
+                Console.WriteLine("Keep up the good work!");
+            }
+
+
+            // let's assign letter grades
+            // if mark is at least 80: A
+            if (mark >= 80)
+            { letterGrade = 'A'; }
+            // if mark is at least 70: B
+            else if (mark >= 70)
+            { letterGrade = 'B'; }
+            // if mark is at least 60: C
+            else if (mark >= 60)
+            { letterGrade = 'C'; }
+            // if mark is at least 50: D
+            else if (mark >= 50)
+            {letterGrade = 'D'; }
+            // otherwise, F
+            else { letterGrade = 'F'; }
+
+            Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+            Console.WriteLine("Goodbye.");
+
+        }
+    }
+}
---
+++ Fall 2023 - Section 5/SandboxA05/Sep27IfStatements/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Sep27IfStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            string userResponse;
+            int userFave, 
+                weather;
+
+            // welcome the user
+            Console.WriteLine("Welcome to my program. " +
+                "Please enter your favourite chocolate. " +
+                "Enter 1 for milk, 2 for white, 3 for dark.");
+
+            // prompt them for their favourite type of chocolate
+            userResponse = Console.ReadLine();
+            userFave = int.Parse(userResponse);
+
+            // if they don't say "dark" then we heckle them
+            if (userFave == 3)
+            {
+                Console.WriteLine("Yes, it's delicious.");
+            }
+            else
+            {
+                Console.WriteLine("I disagree.");
+            } // end of else block
+
+            // prompt the user to tell us the future
+            Console.WriteLine("What will the weather be tomorrow? " +
+                "1 = Sunny, 2 = Cloudy, 3 = Rainy");
+            userResponse = Console.ReadLine();
+            weather = int.Parse(userResponse);
+
+            // if it's sunny, say "YAY!"
+            if (weather == 1)
+            {
+                Console.WriteLine("yay!".ToUpper());
+            }
+            // if it's cloudy, we say "meh"
+            else if (weather == 2)
+            {
+                Console.WriteLine("Meh.");
+            }
+            // if it's rainy, we say "oops"
+            else if (weather == 3)
+            {
+                Console.WriteLine("oops.");
+            }
+            // or if they pick an invalid number:
+            else
+            {
+                Console.WriteLine("That's not a weather type, you FOOL!");
+            }
+
+
+
+
+
+
+        } // end of Main()
+    } // end of my class
+} // end of program
