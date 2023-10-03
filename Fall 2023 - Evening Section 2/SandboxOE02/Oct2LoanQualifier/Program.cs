2534f3218dd1403a731f16ada9d25778ca1d0144
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-10-02 21:44:53 -0600

    Oct 2 class examples

---
+++ Fall 2023 - Evening Section 2/SandboxOE02/Oct2LoanQualifier/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Oct2LoanQualifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            int salary,
+                numYears;
+
+            Console.WriteLine("Welcome to our Super Official Loan Qualifier 3000.");
+
+            Console.Write("Please enter your income in $: ");
+            salary = int.Parse(Console.ReadLine());
+
+            if (salary >= 30000)
+            {
+                Console.Write("How many years have you been at your current job (whole numbers only): ");
+                numYears = int.Parse(Console.ReadLine());
+
+                if (numYears >= 2)
+                {
+                    Console.WriteLine("Congrats! You qualify for our loans with ridiculous interest rates!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you need to have 2 years of experience.");
+                } // end of inner-else block
+
+            } // end of outer if-block
+            else
+            {
+                Console.WriteLine("Sorry, minimum salary of $30k is required.");
+            } // end of outer-else block
+
+        } // end of Main()
+    } // end of class
+} // end of namespace
---
+++ Fall 2023 - Section 5/SandboxA05/Oct2LoanQualifier/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Oct2LoanQualifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            double salary;
+            int years;
+
+            Console.WriteLine("Welcome to our Excellent Loan Advisor Program.");
+
+            Console.Write("Please enter your salary: ");
+            salary = Convert.ToDouble(Console.ReadLine());
+
+            if (salary >= 30000)
+            {
+                Console.Write("Please enter # of years as a whole number: ");
+                years = Convert.ToInt32(Console.ReadLine());
+
+                if (years >= 2)
+                {
+                    Console.WriteLine("Congrats! You are qualified!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you need more experience.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you need a higher income.");
+            } // end of outer else
+
+        }
+    }
+}
