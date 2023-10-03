3e065e03c444ffd507e448ad4315dd61192216f5
Dana <63482575+mxmarsh@users.noreply.github.com>
2023-10-03 09:50:44 -0600

    Oct 3 class examples

---
+++ Fall 2023 - Section 4/SandboxA04/Oct3DoWhile/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Oct3DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char userContinue;
+
+            do
+            {
+                // display menu & prompt user for their choice
+                Console.WriteLine("Welcome to the 8am Tired Class Example.\n" +
+                    "Please select your activity:\n" +
+                    "Generate a [R]andom number\n" +
+                    "Do a Math [Q]uiz");
+                char userChoice = char.Parse(Console.ReadLine());
+
+                // branch according to their choice
+                switch (Char.ToUpper(userChoice)) // switch on uppercase value of userChoice
+                {
+                    case 'R':
+                        // generate a random #
+                        Console.WriteLine("Your random # is 42."); // TO-DO
+                        break;
+                    case 'Q':
+                        // give them a Math question
+                        Console.WriteLine("Math is under construction."); // TO-DO
+                        break;
+                    default:
+                        Console.WriteLine("That is not a valid option.");
+                        break;
+                }
+
+                // ask the user if they want to continue
+                Console.Write("Would you like to play again? [Y]es or [N]o: ");
+                userContinue = char.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+            } while (userContinue == 'Y' || userContinue == 'y');
+
+            Console.WriteLine("Thanks for playing. Goodbye.");
+
+        }
+    }
+}
---
+++ Fall 2023 - Section 4/SandboxA04/Oct3Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Oct3Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int originalNum,
+                currentNum,
+                result = 1 ;
+
+            Console.Write("Welcome to our Factorial Calculator!\n\n" +
+                "Please enter a #: ");
+            originalNum = int.Parse(Console.ReadLine());
+            currentNum = originalNum;
+
+            while ( currentNum > 1 )
+            {
+                result *= currentNum; // result = result * currentNum
+                currentNum--;         // currentNum = currentNum - 1
+            }
+
+            Console.WriteLine($"The value of {originalNum}! is {result}.");
+
+
+        }
+    }
+}
