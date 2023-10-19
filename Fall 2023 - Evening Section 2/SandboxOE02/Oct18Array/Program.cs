2a9a41a820b90a04a833ab4e3cb3147ee58ee696
Dana <dmarsh@nait.ca>
2023-10-18 21:42:31 -0600

    Oct 18 class examples

---
+++ Fall 2023 - Evening Section 2/SandboxOE02/Oct18Array/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Oct18Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ARRAY_SIZE = 7;
+            string userResponse;
+
+            do
+            {
+                bool isWinner = false;
+
+                // create an array of ints
+                int[] lottoTicket = new int[ARRAY_SIZE];
+
+                // ask the user to provide their lotto picks
+                for (int index = 0; index < lottoTicket.Length; index++)
+                {
+                    // prompt the user for a number
+                    Console.Write("Please enter a number: ");
+
+                    bool isInvalid = true; // or isValid = false
+
+                    do // starts the user-input validation loop
+                    {   // try/catch to make sure we can parse the number
+                        try
+                        {
+                            // save the number into our array
+                            lottoTicket[index] = int.Parse(Console.ReadLine());
+
+                            // make sure the number is between 1 and 100
+                            if (lottoTicket[index] < 1)
+                                Console.WriteLine("Must be a positive number. Try again.");
+                            else if (lottoTicket[index] > 100)
+                                Console.WriteLine("Too high, please try again.");
+                            else
+                                isInvalid = false;    // or isValid = true;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Invalid: please enter a #.");
+                        }
+                    } while (isInvalid);  // or while (!isValid)
+                                          // ends the user-input validation loop
+
+                } // end of my for-loop
+
+                // generate a random number
+                Random numGenerator = new Random();
+                int winningNumber = numGenerator.Next(1, 10); // at least 1 & less than 101
+                Console.WriteLine("The winning number is " + winningNumber);
+
+                // loop through the user's picks, display them,
+                // Your lotto numbers are: 1, 2, 3, 4, 5, 6, 7
+                Console.Write("Your lotto numbers are: ");
+                for (int index = 0; index < lottoTicket.Length; index++)
+                {
+                    Console.Write(lottoTicket[index]);
+
+                    // for every element EXCEPT the last one, we will print a comma
+                    if (index < lottoTicket.Length - 1)
+                        Console.Write(", ");
+
+                    // and determine if they are a winner
+                    if (!isWinner && (lottoTicket[index] == winningNumber))
+                    {
+                        isWinner = true;
+                    }
+
+                } // end of display loop
+
+                if (isWinner)
+                    Console.WriteLine("\n\nCongrats! You are rich and can swim in a pile of gold coins.");
+                else
+                    Console.WriteLine("\n\nSorry, you did not win.");
+
+                Console.Write("Would you like to buy another ticket?\n" +
+                    "Enter Y for Yes and anything else to Exit: ");
+                 userResponse = Console.ReadLine();
+
+            } while (userResponse.ToUpper().Equals("Y"));
+
+            Console.WriteLine("Thanks for playing, goodbye.");
+
+            // TO DO: turn min & max numbers into constants
+        }
+    }
+}
---
+++ Fall 2023 - Section 5/SandboxA05/Oct18Array/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Oct18Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ARRAY_SIZE = 10;
+
+            // create an empty array that will contain Pokemon
+            string[] myPokemon = new string[ARRAY_SIZE];
+
+            // load the array with values
+            for (int index = 0; index < myPokemon.Length; index++)
+            {
+                // ask them for a Pokemon
+                Console.Write("Please enter a Pokemon: ");
+                // save that to the current location
+                myPokemon[index] = Console.ReadLine();
+            }
+
+            // display the values in the array
+            // Your Pokemon are Pikachu, Charmander, Squirtle, ...
+
+            // display a random Pokemon from the array
+            // today's lucky Pokemon is .....
+
+            // maybe we will search the array?
+            // ask the user for a Pokemon, and we will tell them
+            // if it is in the array already
+            
+        }
+    }
+}
