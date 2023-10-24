8cf8ab13d832a4fb6bc1a7df83ccba921eb26bee
Dana <dmarsh@nait.ca>
2023-10-23 18:23:09 -0600

    Oct 23 class examples

---
+++ Fall 2023 - Section 4/SandboxA04/Oct23Method/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Oct23Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.");
+
+            DoNothing();
+
+            int daysTillHalloween = GetHalloweenCountdown();
+            Console.WriteLine($"There are {daysTillHalloween} days till Hallowe'en.");
+
+            Console.WriteLine($"There are {GetHalloweenCountdown} days till Hallowe'en.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Goodbye.");
+        }
+
+        static void DoNothing()
+        {
+            Console.WriteLine("Nothing to see here.");
+        }
+
+        static int GetHalloweenCountdown()
+        {
+            return 8; // TO DO: Dana to un-hardcode this
+        }
+
+    }
+}
+
+/* what we'd like to practice:
+ * loops
+ * exceptions
+ * Math class
+ * arrays!!!
+ */
---
+++ Fall 2023 - Section 5/SandboxA05/Oct23Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Oct23Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            PrintMessage();
+            PrintInPink("hey this is a message I wrote.");
+            Console.WriteLine("Goodbye.");
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine("Inconceivable!");
+        }
+
+        static void PrintInPink(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+    }
+}
+
+/* practice:
+ * xor
+ * formatting output
+ * Math
+ */
