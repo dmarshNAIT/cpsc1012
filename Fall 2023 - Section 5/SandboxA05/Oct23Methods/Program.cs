e67a4caefe0101ffd68597a59f69706b475e0098
Dana <dmarsh@nait.ca>
2023-10-25 16:00:11 -0600

    Oct 25 class examples

--- Fall 2023 - Section 5/SandboxA05/Oct23Methods/Program.cs
+++ Fall 2023 - Section 5/SandboxA05/Oct23Methods/Program.cs
@@ -7,6 +7,9 @@
             Console.WriteLine("Hello!");
             PrintMessage();
             PrintInPink("hey this is a message I wrote.");
+            Console.WriteLine($"I predict tomorrow's weather is {PredictWeather()} degrees.");
+            Console.WriteLine($"I predict tomorrow's weather is {PredictWeather("winter")} degrees");
+
             Console.WriteLine("Goodbye.");
         }
 
@@ -21,6 +24,38 @@
             Console.WriteLine(message);
             Console.ResetColor();
         }
+
+        static int PredictWeather()
+        {
+            Random numGenerator = new Random();
+            return numGenerator.Next(-40, 36);
+        }
+
+        static int PredictWeather(string season)
+        {
+            Random numGenerator = new Random();
+            int randomNum;
+
+            switch (season)
+            {
+                case "summer":
+                    randomNum = numGenerator.Next(10, 41);
+                    break;
+                case "spring":
+                case "fall":
+                    randomNum = numGenerator.Next(-5, 21);
+                    break;
+                case "winter":
+                    randomNum = numGenerator.Next(-40, 6);
+                    break;
+                default:
+                    randomNum = 0;
+                    break;
+            }
+
+            return randomNum;
+        }
+
 
     }
 }
