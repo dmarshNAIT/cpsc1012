63cf7015d1371c48a386b9a7f5a1129d148b5620
Dana <dmarsh@nait.ca>
2023-11-08 19:45:54 -0700

    Nov 8 asn 3 examples

---
+++ Fall 2023 - Evening Section 2/SandboxOE02/Nov8Asn3Practice/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Nov8Asn3Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 5, 2, 5, 6, 3, 4, 3, 8, 4, 9 };
+            int result;
+
+
+            result = SearchArray(ints, 2);
+            Console.WriteLine($"We found that number {result} times.");
+
+        }
+
+        static int SearchArray(int[] array, int numberToFind)
+        {
+            // initialize counter 
+            int counter = 0;
+
+            // loop thru the elements in the array
+            for (int i = 0; i < array.Length; i++)
+            {
+                // if they match what I am looking for, then we increment the counter
+                if(numberToFind == array[i])
+                    counter++;
+            }
+
+            // return the counter
+            return counter;
+        }
+    }
+}
