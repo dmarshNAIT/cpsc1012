ed8b2908f4ad5c75f964ac06439116a6209ebd19
Dana <dmarsh@nait.ca>
2023-11-13 10:07:43 -0700

    Added DisplayChart() example

---
+++ BoxDrawingExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace TestBox
+{
+    internal class BoxDrawingExample
+    {
+        static void Main(string[] args)
+        {
+            DrawRectangle(50,17);
+        }
+
+        static void DrawRectangle(int numColumns, int numRows)
+        {
+            string box = "";
+
+            // print top row
+            box += DrawStars(numColumns);
+            box += "\n";
+
+            // print contents
+            for (int line = 0; line < numRows - 2; line++ ){
+                // print asterisk
+                box += "*";
+
+                // print a bunch of spaces
+                for(int col = 0; col < numColumns -2; col++)
+                {
+                    box += " ";
+                }
+
+                // print another asterisk
+                box += "*\n";
+            }
+
+            // print bottom row
+            box += DrawStars(numColumns);
+            Console.WriteLine(box);
+        }
+
+        static string DrawStars(int numColumns)
+        {
+            string box = "";
+            for (int col = 0; col < numColumns; col++)
+            {
+                box += "*";
+            }
+            return box;
+        }
+
+    }
+
+}
---
+++ Fall 2023 - Section 5/SandboxA05/Nov9MethodPractice/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Nov9MethodPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicNumber = 4;
+
+            // get input from the user
+            GetInput();
+            // calculate something
+            int result = CalculateSomething(magicNumber);
+            // display results
+            DisplayStuff();
+
+
+            // create a variable
+            int myInt = 15;
+
+
+            // create a brand new array
+            int[] dfsilsdflkjdfs = new int[5];
+
+            int[] andy = { 5, 4, 3, 2, 6 };
+
+            for(int i = 0; i < andy.Length; i++)
+            {
+                Console.WriteLine($"The value in my array is {andy[i]}");
+            }
+        }
+
+        static void GetInput()
+        {
+
+        }
+
+        static int CalculateSomething(int numDays)
+        {
+            int numHours = numDays * 24;
+
+            return numHours;
+        }
+
+        static void DisplayStuff()
+        {
+
+        }
+    }
+}
