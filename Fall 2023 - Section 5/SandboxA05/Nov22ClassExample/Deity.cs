d8ee5a7c846ecd9f08b1a85064f01827f2f6e43f
Dana <dmarsh@nait.ca>
2023-11-22 16:43:53 -0700

    Nov 22 class examples

---
+++ Fall 2023 - Section 5/SandboxA05/Nov22ClassExample/Deity.cs
@@ -0,0 +1,25 @@
+﻿namespace Nov22ClassExample
+{
+    internal class Deity
+    {
+        // fields
+        private string _name = "Unknown";
+        private bool _isOmniscient; // all-knowing
+        private bool _isOmnipotent; // all-powerful
+        private int _numberOfFollowers = 1;
+
+        // methods
+        // no-argument constructor
+        public Deity() { }
+
+        // parameterized constructor
+        public Deity(string name, bool isOmniscient, bool isOmnipotent, int numberOfFollowers) { 
+        
+        }
+
+        // optional homework: create getter & setter method for all fields
+
+        // Smite();
+        // Inspire();
+    }
+}
---
+++ Fall 2023 - Section 5/SandboxA05/Nov22ClassExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Nov22ClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new object
+            Deity defaultDeity = new Deity();
+
+
+        }
+    }
+}
