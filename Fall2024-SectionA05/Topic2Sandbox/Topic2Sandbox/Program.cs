dbebe3d5bbf68ea806df41d1a96681f4664e0d82
Dana Marsh <dmarsh@nait.ca>
2024-09-09 12:05:16 -0600

    Sep 9 in-class examples

---
+++ Fall2024-SectionA05/Topic2Sandbox/Topic2Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic2Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your 1st number.");
+            Console.Write("Enter your 2nd number.");
+            Console.WriteLine("Here is your solution.");
+
+            int bob = 7;
+            Console.WriteLine("The value is " + bob + ".");
+
+            double pi = 3.14;
+        }
+    }
+}
