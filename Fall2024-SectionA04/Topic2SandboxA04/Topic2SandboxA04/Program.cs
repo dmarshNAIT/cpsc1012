8c44168a29e88bc09143b7707f2a55625f51283a
Dana Marsh <dmarsh@nait.ca>
2024-09-09 15:02:53 -0600

    Added Sep 9 class examples

---
+++ Fall2024-SectionA04/Topic2SandboxA04/Topic2SandboxA04/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Topic2SandboxA04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yay, petting zoos.");
+            Console.Write("Welcome!");
+            Console.WriteLine("$900 to enter.");
+
+            // let's create a variable:
+            int pettingZoo = 99;
+            Console.WriteLine("There are " + pettingZoo + " animals in the zoo.");
+
+            double feedPrice = 9.95;
+            Console.WriteLine("Feed costs $" + feedPrice);
+        }
+    }
+}
