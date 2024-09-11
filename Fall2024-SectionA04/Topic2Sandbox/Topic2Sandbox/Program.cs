520be6fe9f15acc5cda52016a07e2a6d8be62faf
Dana Marsh <dmarsh@nait.ca>
2024-09-11 15:10:38 -0600

    Sep 11 class examples

---
+++ Fall2024-SectionA04/Topic2Sandbox/Topic2Sandbox/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Topic2Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yay, petting zoos.");
+            Console.Write("Welcome!");
+            Console.WriteLine("$900 to enter.");
+
+            // let's create a variable:
+            int numberOfAnimals = 99;
+            Console.WriteLine("There are " + numberOfAnimals + " animals in the zoo.");
+
+            double feedPrice = 9.95;
+            Console.WriteLine("Feed costs $" + feedPrice);
+
+            // doing math
+            int numberOfStudents = 2;
+            double piecesOfCandy = 5;
+            // if BOTH operands are ints, the result will ALWAYS be an int. That's why we made one of the operands a double, instead.
+            double candyPerStudent = piecesOfCandy / numberOfStudents;
+
+            Console.WriteLine("Each student gets " + candyPerStudent + " pieces of candy.\n\n\n\n\n\n");
+
+            double result = 2 / 7.0;
+            Console.WriteLine("The answer is " + result);
+
+            double roundedResult = Math.Round(result, 3);
+            Console.WriteLine("The rounded answer is " + roundedResult);
+
+            double myGrade = 84;
+            int myGradeWholeNumber = (int) 84.2;
+            Console.WriteLine(myGradeWholeNumber);
+
+            // messing around with data types (DON'T DO THIS)
+            double x = -3.14159;
+            byte y = (byte) x;
+            Console.WriteLine(y);
+
+        }
+    }
+}
