ce30f15a5176ba933f4376827a6c84797d5f824f
Dana Marsh <dmarsh@nait.ca>
2024-10-09 14:02:22 -0600

    Add Average Mark calculator v1 🧮

---
+++ Fall2024-SectionA04/Topic4LoopsPractice/Topic4LoopsPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ *  TO DO: Dana write things here
+ */
+namespace Topic4LoopsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            int numberOfMarks;
+            string className; // TO DO: scope
+            double sum = 0;
+
+            // welcome 
+            Console.WriteLine("Welcome!");
+
+            // prompt user for numberOfMarks & className
+            Console.Write("Enter the course name: ");
+            className = Console.ReadLine();
+            Console.Write("How many marks will you enter: ");
+            numberOfMarks = int.Parse(Console.ReadLine());
+
+            // start loop
+            for (int counter = numberOfMarks; counter > 0; counter--)
+            {
+                double mark;
+                Console.Write("Please enter mark: ");
+                mark = double.Parse(Console.ReadLine());
+                sum += mark;
+            }
+            // prompt the user for each mark
+            // end loop
+
+            // calculate average
+            // display average
+            Console.WriteLine($"Your average mark for {className} is {sum/numberOfMarks}.");
+
+            // ask the user if they want to go again
+
+        }
+    }
+}
