44c8883371ab8f5c2a1999ba8f1435c0ccb824cf
Dana <dmarsh@nait.ca>
2024-10-10 15:48:59 -0600

    Add outer loop to Average Mark Calculator 🧮

--- Fall2024-SectionA04/Topic4LoopsPractice/Topic4LoopsPractice/Program.cs
+++ Fall2024-SectionA04/Topic4LoopsPractice/Topic4LoopsPractice/Program.cs
@@ -7,36 +7,52 @@ namespace Topic4LoopsPractice
     {
         static void Main(string[] args)
         {
-            // declare variables
-            int numberOfMarks;
-            string className; // TO DO: scope
-            double sum = 0;
+            // declare variables needed for whole program:
+            string userChoice;
 
             // welcome 
             Console.WriteLine("Welcome!");
 
-            // prompt user for numberOfMarks & className
-            Console.Write("Enter the course name: ");
-            className = Console.ReadLine();
-            Console.Write("How many marks will you enter: ");
-            numberOfMarks = int.Parse(Console.ReadLine());
-
             // start loop
-            for (int counter = numberOfMarks; counter > 0; counter--)
+            do
             {
-                double mark;
-                Console.Write("Please enter mark: ");
-                mark = double.Parse(Console.ReadLine());
-                sum += mark;
-            }
-            // prompt the user for each mark
+                // declare variables needed in loop:
+                int numberOfMarks;
+                string className; 
+                double sum = 0;
+
+                // prompt user for numberOfMarks & className
+                Console.Write("\nEnter the course name: ");
+                className = Console.ReadLine();
+                Console.Write("How many marks will you enter: ");
+
+                // TO DO: add input validation
+                numberOfMarks = int.Parse(Console.ReadLine());
+
+                // start loop
+                for (int counter = numberOfMarks; counter > 0; counter--)
+                {
+                    double mark;
+                    Console.Write("Please enter mark: ");
+
+                    // TO DO: add input validation
+                    mark = double.Parse(Console.ReadLine());
+
+                    sum += mark;
+                }
+                // end loop
+
+                // calculate & display average
+                Console.WriteLine($"Your average mark for {className} is {sum / numberOfMarks}.");
+
+                // ask the user if they want to go again
+                Console.Write("\nWould you like to do another calculation? \nEnter C to continue or any other key to exit: ");
+                userChoice = Console.ReadLine().ToUpper();
+
+            } while (userChoice == "C");
             // end loop
 
-            // calculate average
-            // display average
-            Console.WriteLine($"Your average mark for {className} is {sum/numberOfMarks}.");
-
-            // ask the user if they want to go again
+            Console.WriteLine("Thanks, bye!");
 
         }
     }
