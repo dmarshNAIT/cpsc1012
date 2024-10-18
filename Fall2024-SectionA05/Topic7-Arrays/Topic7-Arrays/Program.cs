f4d506b2a5fde0586488f8ea9a8f0fe215082b67
Dana <dmarsh@nait.ca>
2024-10-17 20:12:07 -0600

    Add placeholder array project 🔢

---
+++ Fall2024-SectionA05/Topic7-Arrays/Topic7-Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic7_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {12 + 6 / 2}");
+        }
+    }
+}
