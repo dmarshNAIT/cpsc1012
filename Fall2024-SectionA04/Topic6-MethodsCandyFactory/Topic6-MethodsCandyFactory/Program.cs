887ab2d71b488b6d109e9c74c1d32d09fed7a21b
Dana <dmarsh@nait.ca>
2024-10-24 16:31:44 -0600

    Update Candy Factory 🍬🍭🍫

--- Fall2024-SectionA04/Topic6-MethodsCandyFactory/Topic6-MethodsCandyFactory/Program.cs
+++ Fall2024-SectionA04/Topic6-MethodsCandyFactory/Topic6-MethodsCandyFactory/Program.cs
@@ -1,28 +1,86 @@
 ﻿
+
 namespace Topic6_MethodsCandyFactory
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // TODO: declare variables
+            const int PHYSICAL_SIZE = 13; //because Hallowe'en
+            
             char menuChoice;
+            string[] candyBucket = new string[PHYSICAL_SIZE]; // names of our candy
+            int[] candyCount = new int[PHYSICAL_SIZE]; // inventory level per candy
 
             do
             {
-                // TODO: display menu
                 DisplayMenu();
-
-                // TODO: get the user's input
                 menuChoice = GetValidChar();
-
-                // TODO: call the appropriate method
-                //HandleUserChoice(menuChoice);
+                HandleUserChoice(menuChoice, candyBucket, candyCount);
 
             } while (menuChoice != '4');
 
             Console.WriteLine("Thank you, goodbye.");
+        }
 
+        static void DisplayMenu()
+        {
+            Console.WriteLine("MAIN MENU\n" +
+                "\t1. Populate the arrays\n" +
+                "\t2. Display inventory\n" +
+                "\t3. Display total count\n" +
+                "\t4. Quit\n");
+        }
+
+        static void HandleUserChoice(char choice, string[] candy, int[] inventory)
+        {
+            switch (choice)
+            {
+                case '1':
+                    PopulateArrays(candy, inventory);
+                    break;
+                case '2':
+                    DisplayInventory();
+                    break;
+                case '3':
+                    DisplayTotalInventoryCount();
+                    break;
+                case '4':
+                    // do nothing
+                    break;
+                default:
+                    Console.WriteLine("That's not an option.");
+                    break;
+            }
+        }
+
+        static void PopulateArrays(string[] candy, int[] inventory)
+        {
+            Console.WriteLine("put things in arrays now");
+            //TODO: complete method
+
+            // OPTION A:
+            // ask the user how many types of candy they have
+            // loop through, ask them for each candy name & inventory level
+
+            // YOUR CHALLENGE: code this!
+
+            // alternate approach we could have taken:
+            // ask them to enter a candy name, OR enter 0 to quit
+            // then they enter the inventory
+        }
+
+        static void DisplayInventory()
+        {
+            Console.WriteLine("we are displaying inventory!");
+            //TODO: complete method
+        }
+
+        static void DisplayTotalInventoryCount()
+        {
+            Console.WriteLine("we are in the display total inventory method!");
+            //TODO: complete method
+            // TODO: suggest re-stock if inventory levels are low
         }
 
         static char GetValidChar()
@@ -52,15 +110,6 @@ namespace Topic6_MethodsCandyFactory
 
             return userResponse;
         } // end of method
-
-        static void DisplayMenu()
-        {
-            // Main Menu
-            // 1. Populate the arrays
-            // 2. Display inventory
-            // 3. Display total count (& recommend re-stock)
-            // 4. Quit
-        }
 
     } // end of class
 } // end of namespace
