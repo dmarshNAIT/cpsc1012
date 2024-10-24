bf80e2ba4ed02b18131034df59ae18019159d7dd
Dana <dmarsh@nait.ca>
2024-10-24 11:45:39 -0600

    Add silly methods example 🧛

---
+++ Fall2024-SectionA05/Topic6-MethodsSandbox/Topic6-MethodsSandbox/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Mail;
+
+namespace Topic6_MethodsSandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeUser();
+            int[] myValues = GetWinningLotteryNumbers();
+
+            int total = GetSumOfNumbers(myValues);
+            Console.WriteLine("The total is " + total);
+        }
+
+        // void method w/ no params
+        static void WelcomeUser()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello, fellow vampire.");
+            Console.ResetColor();
+        }
+
+        // method w/ no params that returns something
+        static int[] GetWinningLotteryNumbers()
+        {
+            int[] winningNumbers = { 47, 8, 23, 30, 32, 33, 49};
+            return winningNumbers;
+        }
+
+        // method w/ params
+        static int GetSumOfNumbers(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+    }
+}
