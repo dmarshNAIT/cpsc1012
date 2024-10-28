d0107bb9eebba374de0dc905103fb6103b0a5511
Dana Marsh <dmarsh@nait.ca>
2024-10-28 11:33:58 -0600

    Add structure for Handler method 👞

--- Fall2024-SectionA05/Topic6-ShoeStore/Topic6-ShoeStore/Program.cs
+++ Fall2024-SectionA05/Topic6-ShoeStore/Topic6-ShoeStore/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+//TODO: DOCUMENTATION
 
 namespace Topic6_ShoeStore
 {
@@ -7,12 +8,17 @@ namespace Topic6_ShoeStore
         static void Main(string[] args)
         {
             int userChoice;
+            const int PHYSICAL_SIZE = 50;
+            string[] brandz = new string[PHYSICAL_SIZE];
+            int[] shoeLengths = new int[PHYSICAL_SIZE];
+            int[] ukSizes = new int[PHYSICAL_SIZE];
+            int logicalSize = 0;
 
             do
             {
                 DisplayMainMenu();
                 userChoice = GetValidInt();
-                //HandleUserChoice(userChoice);
+                HandleUserChoice(userChoice, brandz, shoeLengths, ukSizes, logicalSize);
 
             } while (userChoice != 5);
             // userChoice is not 5
@@ -20,7 +26,6 @@ namespace Topic6_ShoeStore
 
             Console.WriteLine("Thanks, goodbye!");
         }
-
 
         static void DisplayMainMenu()
         {
@@ -30,6 +35,37 @@ namespace Topic6_ShoeStore
                 "\t3. Save to File\n" +
                 "\t4. Import from File\n" +
                 "\t5. Quit\n");
+        }
+
+        static void HandleUserChoice(int userChoice)
+        {// TODO: update our parameters
+            switch (userChoice)
+            {
+                case 1:
+                    logicalSize = FillArrays(brandz, shoeLengths, ukSizes, logicalSize);
+                    // uk size = 3 * length - 23
+                    // this method will use a sentinel value
+                    Console.WriteLine("filling arrays");
+                    break;
+                case 2:
+                    //DisplayArrays();
+                    Console.WriteLine("displaying arrays");
+                    break;
+                case 3:
+                    //SaveToFile();
+                    Console.WriteLine("saving");
+                    break;
+                case 4:
+                    //ReadFromFile();
+                    Console.WriteLine("loading");
+                    break;
+                case 5:
+                    // do nothing
+                    break;
+                default:
+                    Console.WriteLine("That's not an option.");
+                    break;
+            }
         }
 
         static int GetValidInt()
@@ -53,12 +89,10 @@ namespace Topic6_ShoeStore
             } while (!isValid);
 
             return inputValue;
-        }
-    }
+        } // end of the method
+    } // end of the clas
 }
 
 
 // TODO:
-// Create Handler method which will branch based on the user's selection
-
 // arrays: one for brands, one for length, one for sizes
