cae273f6bb814a2acb5ce0f350b6c902b1cb8d2d
Dana <dmarsh@nait.ca>
2024-11-21 16:21:25 -0700

    Add constructors & properties :house:

--- Fall2024-SectionA04/Topic8-BobTheBuilder/Topic8-BobTheBuilder/House.cs
+++ Fall2024-SectionA04/Topic8-BobTheBuilder/Topic8-BobTheBuilder/House.cs
@@ -8,24 +8,87 @@ namespace Topic8_BobTheBuilder
 {
     internal class House
     {
-        // determine data types, names, initial values, and what is static
+        // static fields would go first, if we had any
+
+        // instance fields, AKA member variables:
+        // determine data types, names, initial values
+        // visibility should always be private
         private int _numberRooms; // ints default to 0
         private int _numberFloors = 1;
         private double _temperature = 19;
         private bool _hasGarage;  // bools default to false
 
-        // TO DO:
-        // add 2+ constructors: one with no parameters, one with all of them
-        // add accessors for every variable
-        // add mutators for every variable
+        // no-argument constructor:
+        public House() { }
+
+        // greedy constructor: all the params!
+        public House(int numberRooms, int numberFloors, double temperature, bool hasGarage)
+        {
+            _numberRooms = numberRooms;
+            _numberFloors = numberFloors;
+            _temperature = temperature;
+            _hasGarage = hasGarage;
+        }
+
+        public int NumberRooms
+        {
+            get { return _numberRooms; }
+            set
+            {
+                if (value > 0) { _numberRooms = value; }
+                else { throw new Exception("Rooms must be > 0."); }
+            }
+        }
+        public int NumberFloors
+        {
+            get { return _numberFloors; }
+            set
+            {
+                if (value > 0) { _numberFloors = value; }
+                else { throw new Exception("Floors must be > 0."); }
+            }
+        }
+
+        // we have 2 ways of providing get/set functionality
+        // option 1: getter & setter methods
+        public double GetTemperature()
+        {
+            return _temperature;
+        }
+        public void SetTemperature(double temperature)
+        {
+            if (temperature <= 0)
+                throw new Exception("Temperature must be above 0.");
+            else if (temperature > 50)
+                throw new Exception("Temperature must be below 50.");
+            else
+                _temperature = temperature;
+        }
+
+        // option 2: properties
+        public double Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Temperature must be above 0.");
+                else if (value > 50)
+                    throw new Exception("Temperature must be below 50.");
+                else
+                    _temperature = value;
+            }
+        }
+
+        public bool HasGarage
+        {
+            get { return _hasGarage; }
+            set { _hasGarage = value; }
+        }
 
         // LATER:
         // add some other methods
-       
+        // Renovate(), Decorate / adding furniture
+
     }
 }
-
-
-// behaviour:
-// constructors, setter, getter
-// Renovate(), Decorate / adding furniture
--- Fall2024-SectionA04/Topic8-BobTheBuilder/Topic8-BobTheBuilder/Program.cs
+++ Fall2024-SectionA04/Topic8-BobTheBuilder/Topic8-BobTheBuilder/Program.cs
@@ -4,8 +4,45 @@
     {
         static void Main(string[] args)
         {
-            // build a house
-            House myMansion = new House();
+            // build some houses
+            House defaultHouse = new House();
+            House fancyHouse = new House(20, 4, 21, true);
+            // order of my ints must match
+            House fancyHouseV2 = new House(numberRooms: 20, numberFloors: 4, 21, true);
+            // optionally, we can use named parameters to improve readability and hopefully reduce confusion
+
+            // test my setter & getter methods
+            fancyHouse.SetTemperature(25);
+            Console.WriteLine("The temperature of my fancy house is " + fancyHouse.GetTemperature());
+
+            // test my properties:
+            fancyHouseV2.Temperature = 18;
+            Console.WriteLine("The temperature of my other fancy house is "  + fancyHouseV2.Temperature);
+
+            string userChoice;
+            do
+            {
+                // TODO before next class:
+                // display menu
+                // create a new object
+                // get # of rooms
+                // set # of rooms
+                // get # of floors
+                // set # of floors
+                // get temp
+                // set temp
+                // see if there is a garage
+                // change if there is a garage
+
+                // don't worry about exception handling in THIS program for now
+
+                // branch 
+
+
+            } while (userChoice != "Q");
+
+
+            Console.WriteLine();
         }
     }
 }
