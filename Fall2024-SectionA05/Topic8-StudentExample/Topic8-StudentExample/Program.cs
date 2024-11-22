408ed00f2c8ea7b406c413598b974e58d378d7f7
Dana Marsh <dmarsh@nait.ca>
2024-11-22 16:53:06 -0700

    Add property & constructor :apple:

--- Fall2024-SectionA05/Topic8-StudentExample/Topic8-StudentExample/Program.cs
+++ Fall2024-SectionA05/Topic8-StudentExample/Topic8-StudentExample/Program.cs
@@ -18,6 +18,13 @@
             maimee.Name = "Maimee";
             Console.WriteLine("The student's name is " + maimee.Name);
 
+            Console.WriteLine("Student number is " + maimee.StudentNumber);
+
+            maimee.Age = 63;
+            Console.WriteLine($"The student is {maimee.Age} years old.");
+
+            Console.WriteLine("Their GPA is " + maimee.GPA);
+
             Console.WriteLine();
         }
     }
--- Fall2024-SectionA05/Topic8-StudentExample/Topic8-StudentExample/Student.cs
+++ Fall2024-SectionA05/Topic8-StudentExample/Topic8-StudentExample/Student.cs
@@ -9,7 +9,7 @@
 
         private string _name = "John Doe";
         private int _studentNumber;
-        private int _age = 18;
+        private byte _age = 18;
         private double _gpa; //default value for a double is 0
 
         //TODO: Parallel arrays: one for class
@@ -17,11 +17,25 @@
 
         //TODO: a couple of constructors
         // no-arg constructor
-        public Student() {
+        public Student()
+        {
             _studentNumber = _identity + 1;
             _identity++;
         }
         // a constructor with lots of parameters
+        public Student(string name, byte age)
+        {
+            // set name
+            Name = name;
+            // using the property means I can re-use its validation and I don't need to repeat myself here
+
+            _studentNumber = _identity + 1;
+            _identity++; //TODO later: un-duplicate
+
+            // set age
+            this.Age = age; // "this" is optional
+            // "this" refers to the current object
+        }
 
         //TODO: getters & setters
         // approach 1: creating getter & setter methods
@@ -30,8 +44,16 @@
             return _name;
         }
 
-        public void SetName(string name) { 
-            _name = name;
+        public void SetName(string name)
+        {
+            if (name.Length > 0 && name.Length <= 100)
+            {
+                _name = name;
+            }
+            else
+            {
+                throw new Exception("Name must be 0-100 characters.");
+            }
         }
 
         // approach 2: creating Properties
@@ -41,10 +63,46 @@
         public string Name
         {
             get { return _name; }
-            set { _name = value; }
+            set
+            {
+                if (value.Length > 0 && value.Length <= 100)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("Name must be 0-100 characters.");
+                }
+            }
         }
-        // TODO: add validation to all getters
-        // TODO : add properties for all the relevant fields
+
+        public int StudentNumber
+        {
+            get { return _studentNumber; }
+        }
+
+        public byte Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 16 && value <= 120)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new Exception("Age must be between 16 and 120.");
+                    // can use a more specific Exception class e.g. ArgumentOutOfRangeException
+                }
+            }
+        }
+
+        public double GPA
+        {
+            get { return _gpa; }
+            // TODO: implement calculation
+        }
 
 
         //LATER TODO: create some other methods to do things
