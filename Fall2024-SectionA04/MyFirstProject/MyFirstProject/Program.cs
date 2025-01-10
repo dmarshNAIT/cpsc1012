0e52b2b9012419d4a2d9470712896f2120dde905
Dana Marsh <dmarsh@nait.ca>
2025-01-10 11:38:56 -0700

    Add class example

---
+++ Fall2024-SectionA04/MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chill guy shrugs shoulders.");
+        }
+    }
+}
