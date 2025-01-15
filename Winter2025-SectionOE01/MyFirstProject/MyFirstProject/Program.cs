96ad2a2390aeb908c602c96239fdbc5ba709a3a5
Dana <dmarsh@nait.ca>
2025-01-14 21:49:09 -0700

    Add our first program! 👋

---
+++ Winter2025-SectionOE01/MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peace be upon you.");
+            Console.Write("What's up?");
+        }
+    }
+}
