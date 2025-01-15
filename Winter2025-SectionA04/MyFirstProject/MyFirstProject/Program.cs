83f7226cc5f7e7026bfda8435059974ddafee55a
Dana Marsh <dmarsh@nait.ca>
2025-01-14 17:57:36 -0700

    Add statement to in-class example 🤷

--- Winter2025-SectionA04/MyFirstProject/MyFirstProject/Program.cs
+++ Winter2025-SectionA04/MyFirstProject/MyFirstProject/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Chill guy shrugs shoulders.");
+            Console.Write("Chill guy shrugs shoulders.");
+            Console.WriteLine("unshrugs\n\n\nshoulders+");
         }
     }
 }
