57325ee5c0314c511e0d69e1243af2f7e7deb544
Dana <dmarsh@nait.ca>
2025-02-05 14:50:08 -0700

    Added variety of conditions to check userChoice

---
+++ Winter2025-SectionA04/LogicalOperatorsPractice/LogicalOperatorsPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LogicalOperatorsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char userChoice;
+
+            Console.Write("Do you want to continue? Y/N: ");
+            userChoice = char.Parse(Console.ReadLine());
+
+            // the user types y OR the user types Y
+            if (userChoice == 'y' || userChoice == 'Y')
+            {
+                Console.WriteLine("V1: The user wants to continue.");
+            }
+
+            // v2:
+            // the user doesn't type N or n
+            if (userChoice != 'n' && userChoice != 'N'){
+                Console.WriteLine("V2: The user wants to continue.");
+            }
+
+            // v3:
+            // it's not true that the user typed 'n' or 'N'
+            if (!(userChoice == 'N' || userChoice == 'n')) {
+                Console.WriteLine("V3: The user wants to continue.");
+            }
+
+            // v4:
+            if (Char.ToUpper(userChoice) == 'Y')
+            {
+                Console.WriteLine("V4: The user wants to continue.");
+            }
+        }
+    }
+}
