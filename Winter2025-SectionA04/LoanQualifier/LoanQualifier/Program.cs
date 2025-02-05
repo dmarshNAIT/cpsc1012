4849934ac3ee92389010be4753842022b8a9fbbb
Dana <dmarsh@nait.ca>
2025-02-05 13:26:53 -0700

    First draft of LoanQualifier

---
+++ Winter2025-SectionA04/LoanQualifier/LoanQualifier/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Purpose: To check if a user is qualified for a loan based on their salary & work experience.
+ * Author: Dana Marsh with assistance from section A04
+ * 
+ * Last modified: Feb 5, 2025
+ */
+namespace LoanQualifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            double salary;
+            int tenure;
+
+            // get input
+            Console.Write("Welcome!\n\nPlease enter your salary in $: ");
+            salary = double.Parse(Console.ReadLine());
+            Console.Write("Please enter the # of years you've been employed: ");
+            tenure = int.Parse(Console.ReadLine());
+
+            // check if the user's salary is high enough
+            if (true)
+            {
+                
+            }
+            // if so: check if the user has been working long enough
+            // if they haven't been working long enough, they aren't qualified
+
+            // if salary isn't high enough: let them know they aren't qualified.
+
+            // output results
+
+
+
+
+        }
+    }
+}
