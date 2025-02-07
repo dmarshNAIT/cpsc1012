ba703c3ec7da5a56c513c8c24a1e185279adbb0b
Dana <dmarsh@nait.ca>
2025-02-06 19:35:07 -0700

    Add pseudocode

---
+++ Winter2025-SectionOE01/LoanQualifierNested/LoanQualifierNested/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Purpose:     To see if a user is qualified for a loan.
+ * Authors:     Dana Marsh (with assistance from OE01)
+ * 
+ * Input:       the user's salary, & the user's work experience
+ * Output:      whether or not they qualify for a loan
+ * 
+ * Modified:    Feb 6, 2025
+ */
+namespace LoanQualifierNested
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            double salary;
+            int yearsOfExperience;
+
+            // prompt user for salary
+            // prompt user for yearsOfExperience
+
+            // first, check if their salary is high enough.
+                // if so, we then check if their experience is long enough.
+                    // if their experience is long enough, congrats!
+                    // otherwise, sorry you do not qualify
+                // otherwise if they don't have experience, sorry you do not quality
+        }
+    }
+}
