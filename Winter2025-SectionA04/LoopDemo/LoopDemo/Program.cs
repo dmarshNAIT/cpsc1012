55b94677bdf1bad0acc56f696130056fa3e2044a
Dana Marsh <dmarsh@nait.ca>
2025-02-11 18:38:29 -0700

    Add silly "chill guy" while loop

---
+++ Winter2025-SectionA04/LoopDemo/LoopDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LoopDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chillGuyNumber = 1;
+
+
+            while(chillGuyNumber <= 10)
+            {
+                Console.WriteLine($"Chill guy #{chillGuyNumber}.");
+                ++chillGuyNumber;
+            }
+        }
+    }
+}
