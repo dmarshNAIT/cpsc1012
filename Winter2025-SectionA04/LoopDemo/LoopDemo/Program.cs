252db2976b1c929acf98fbd3c9aa1fada9a31ab2
Dana <dmarsh@nait.ca>
2025-02-12 14:45:04 -0700

    Add a for loop example

--- Winter2025-SectionA04/LoopDemo/LoopDemo/Program.cs
+++ Winter2025-SectionA04/LoopDemo/LoopDemo/Program.cs
@@ -6,12 +6,19 @@
         {
             int chillGuyNumber = 1;
 
-
             while(chillGuyNumber <= 10)
             {
                 Console.WriteLine($"Chill guy #{chillGuyNumber}.");
                 ++chillGuyNumber;
             }
+            // since we know this runs exactly 10 times, this would be better set up as a for loop
+            // however, for a quick introduction to loops, we'll keep it in
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine("Chill guy gets paid.");
+            }
+
         }
     }
 }
