55adfa3e54548a0a7491195240a2342e2d183111
Dana <dmarsh@nait.ca>
2025-02-13 20:46:45 -0700

    Add input validation example

--- Winter2025-SectionOE01/ExceptionSandbox/ExceptionSandbox/Program.cs
+++ Winter2025-SectionOE01/ExceptionSandbox/ExceptionSandbox/Program.cs
@@ -4,35 +4,38 @@
     {
         static void Main(string[] args)
         {
-            //int topNumber = 5,
-            // bottomNumber = 0;
+            int topNumber = 5,
+             bottomNumber = 0;
 
-            // option 1: avoid the problem
+            //option 1: avoid the problem
 
-            //if (bottomNumber == 0)
-            //{
-            //    Console.WriteLine("Sorry, that's not allowed.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine(topNumber / bottomNumber);
-            //}
+            if (bottomNumber == 0)
+            {
+                Console.WriteLine("Sorry, that's not allowed.");
+            }
+            else
+            {
+                Console.WriteLine(topNumber / bottomNumber);
+            }
 
             // option 2: catch the exception
 
-            //try
-            //{
-            //    // the code in the try is the stuff we want to TRY to run
-            //    // but it's code that MIGHT throw an exception
-            //    // if anything in this block throws an exception,
-            //    // we immediately stop running the try block, and run the catch block instead
-            //    Console.WriteLine(topNumber / bottomNumber);
-            //}
-            //catch
-            //{
-            //    // this only runs IF something went wrong in the try
-            //    Console.WriteLine("Sorry, not allowed.");
-            //}
+            try
+            {
+                // the code in the try is the stuff we want to TRY to run
+                // but it's code that MIGHT throw an exception
+                // if anything in this block throws an exception,
+                // we immediately stop running the try block, and run the catch block instead
+                Console.WriteLine(topNumber / bottomNumber);
+            }
+            catch
+            {
+                // this only runs IF something went wrong in the try
+                Console.WriteLine("Sorry, not allowed.");
+            }
+
+            /***************************************************************/
+            // now, using try/catch to validate user input
 
             int numberOfEntries;
             bool validInput = false; // this must be created outside of the loop, because we want to access it outside of the loop.
