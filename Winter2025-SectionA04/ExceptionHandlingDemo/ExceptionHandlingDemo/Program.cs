08609ba6e0cbbb122dfb6011151e847c37391da9
Dana <dmarsh@nait.ca>
2025-02-14 11:44:20 -0700

    Add example of for loop with another loop inside

---
+++ Winter2025-SectionA04/ExceptionHandlingDemo/ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ExceptionHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int topNumber = 5,
+                bottomNumber = 0;
+
+            // version 1: avoid the problem
+            if (bottomNumber == 0)
+            {
+                Console.WriteLine("Sorry, you can't.");
+            }
+            else
+            {
+                Console.WriteLine(topNumber / bottomNumber);
+            }
+
+            // version 2:
+            try
+            {
+                // we put the potentially problematic code here
+                // if anything throws an Exception, we stop running the try block, and immediately start running the catch block
+                Console.WriteLine(topNumber / bottomNumber);
+            }
+            catch
+            {
+                // this only runs if there was an Exception
+                Console.WriteLine("Sorry, not allowed.");
+            }
+
+            /*****************************************************/
+
+            // we create the variables we need outside the loop:
+            bool isValidNumber = false; 
+            // alternatively you may find the logic easier if you have isInvalid as your bool 
+            // that basically just flips all the trues to false and vice versa
+
+            do
+            {
+                // we create the variables we only need INSIDE the loop:
+                int numberOfCups;
+                Console.Write("Please enter the # of cups of coffee you had today: ");
+                try
+                {
+                    numberOfCups = int.Parse(Console.ReadLine());
+                    isValidNumber = true;
+                    Console.WriteLine("You have had " + numberOfCups + " cups of coffee today.");
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; //  bonus content!
+                    Console.WriteLine("Sorry, that was invalid input. Please try again.");
+                    Console.ResetColor();
+                }
+            } while (!isValidNumber);
+            // our condition could also be written as:  isValidNumber == false
+
+        }
+    }
+}
---
+++ Winter2025-SectionA04/ForTryCatchExample/ForTryCatchExample/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ForTryCatchExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // marks calculator:
+            double sum = 0,
+                averageMark;
+
+            // use a for loop to get input from a user
+            for (int counter = 1; counter <= 3; counter++)
+            {
+                double mark = 0; // initialize this; we will overwrite this value later
+                bool invalidInput = true;
+                do
+                {
+                    Console.Write($"Please enter mark #{counter}: ");
+                    try
+                    {
+                        mark = double.Parse(Console.ReadLine());
+                        invalidInput = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("That is invalid input.");
+                        Console.WriteLine(ex.Message);
+                    }
+                } while (invalidInput);
+
+                sum = sum + mark;
+            }
+
+            averageMark = sum / 3;
+
+            Console.WriteLine("Your average mark is: " + averageMark);
+        }
+    }
+}
