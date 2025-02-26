9ffe81fc1edb69a8b0d8cf8aaa81b09fe24a6de9
Dana <dmarsh@nait.ca>
2025-02-26 13:26:52 -0700

    Add input validation

--- Winter2025-SectionA04/InformationStealer/InformationStealer/Program.cs
+++ Winter2025-SectionA04/InformationStealer/InformationStealer/Program.cs
@@ -23,26 +23,58 @@ namespace InformationStealer
                 try
                 {
                     age = int.Parse(Console.ReadLine());
-                    badInput = true;
+                    if (age > 0)
+                    {
+                        badInput = false; // because we now have GOOD input
+                    }
+                    else // # is negative
+                    {
+                        Console.WriteLine("Invalid input: please enter a positive number.");
+                    }
                 }
                 catch
                 {
-                    Console.WriteLine("Incorrect, please enter a digit.");
+                    Console.WriteLine("Invalid input: please enter a numeric value.");
                 }
-            } while (badInput == true);
+            } while (badInput == true); // keep looping as long as we have bad input
 
+            // reset our control variable:
+            badInput = true;
+
+            do
+            {
+                Console.Write("Please enter your salary: $");
+                try
+                {
+                    salary = double.Parse(Console.ReadLine());
+                    if (salary > 0)
+                    {
+                        badInput = false; // because we now have GOOD input
+                    }
+                    else // # is negative
+                    {
+                        Console.WriteLine("Invalid input: please enter a positive number.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input: please enter a numeric value.");
+                }
+            } while (badInput == true); // keep looping as long as we have bad input
 
             // if their salary is below 5000
-            // donate $200
+            // give $200
             // if their salary is above 7000
             // take $1000
 
             // output those values, nicely aligned
             // then ask if they want to go again
 
+            Console.WriteLine("Thanks, bye!");
+
             // debug
             // add second question
-            Console.WriteLine("Thanks, bye!");
+            // TODO scope: where should each variable be created?
         }
     }
 }
