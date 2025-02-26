44a99f916acbecafe57a4300c69b5a0bff3aff8d
Dana <dmarsh@nait.ca>
2025-02-26 14:33:46 -0700

    Complete InfoStealer v1

--- Winter2025-SectionA04/InformationStealer/InformationStealer/Program.cs
+++ Winter2025-SectionA04/InformationStealer/InformationStealer/Program.cs
@@ -10,92 +10,96 @@ namespace InformationStealer
     {
         static void Main(string[] args)
         {
-            // declare variables:
-            int age = 0;// starting value that will be replaced with user input
-            double salary = 0, // starting value that will be replaced with user input
-                gift; // this represents the NEW balance after we add/remove funds
-            bool restartLoop,
+            string userAnswer;
+
+            do {
+                // declare variables:
+                int age = 0;// starting value that will be replaced with user input
+                double salary = 0, // starting value that will be replaced with user input
+                    gift; // this represents the NEW balance after we add/remove funds
+                bool badInput = true;
+
+                // ask the user for their age & salary
+                do
+                {
+                    Console.Write("Please enter your age: ");
+                    try
+                    {
+                        age = int.Parse(Console.ReadLine());
+                        if (age > 0)
+                        {
+                            badInput = false; // because we now have GOOD input
+                        }
+                        else // # is negative
+                        {
+                            Console.WriteLine("Invalid input: please enter a positive number.");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid input: please enter a numeric value.");
+                    }
+                } while (badInput == true); // keep looping as long as we have bad input
+
+                // reset our control variable:
                 badInput = true;
 
-            // ask the user for their age & salary
-            do
-            {
-                Console.Write("Please enter your age: ");
-                try
+                do
                 {
-                    age = int.Parse(Console.ReadLine());
-                    if (age > 0)
+                    Console.Write("Please enter your salary: $");
+                    try
                     {
-                        badInput = false; // because we now have GOOD input
+                        salary = double.Parse(Console.ReadLine());
+                        if (salary > 0)
+                        {
+                            badInput = false; // because we now have GOOD input
+                        }
+                        else // # is negative
+                        {
+                            Console.WriteLine("Invalid input: please enter a positive number.");
+                        }
                     }
-                    else // # is negative
+                    catch
                     {
-                        Console.WriteLine("Invalid input: please enter a positive number.");
+                        Console.WriteLine("Invalid input: please enter a numeric value.");
                     }
-                }
-                catch
+                } while (badInput == true); // keep looping as long as we have bad input
+                                            // if we adjusted our logic a bit, we could also use other conditions like:
+                                            // while (salary <= 0)
+                                            // while (goodInput == false)
+                                            // while (!goodInput)
+                                            // while (badInput)...
+
+                // if their salary is below 5000
+                // give $200
+                if (salary < 5000)
                 {
-                    Console.WriteLine("Invalid input: please enter a numeric value.");
+                    gift = salary + 200;
                 }
-            } while (badInput == true); // keep looping as long as we have bad input
-
-            // reset our control variable:
-            badInput = true;
-
-            do
-            {
-                Console.Write("Please enter your salary: $");
-                try
+                // if their salary is above 7000
+                // take $1000
+                else if (salary > 7000)
                 {
-                    salary = double.Parse(Console.ReadLine());
-                    if (salary > 0)
-                    {
-                        badInput = false; // because we now have GOOD input
-                    }
-                    else // # is negative
-                    {
-                        Console.WriteLine("Invalid input: please enter a positive number.");
-                    }
+                    gift = salary - 1000;
                 }
-                catch
+                else
                 {
-                    Console.WriteLine("Invalid input: please enter a numeric value.");
+                    gift = salary;
                 }
-            } while (badInput == true); // keep looping as long as we have bad input
-            // if we adjusted our logic a bit, we could also use other conditions like:
-            // while (salary <= 0)
-            // while (goodInput == false)
-            // while (!goodInput)
-            // while (badInput)...
 
-            // if their salary is below 5000
-            // give $200
-            if (salary < 5000)
-            {
-                gift = salary + 200;
-            }
-            // if their salary is above 7000
-            // take $1000
-            else if (salary > 7000)
-            {
-                gift = salary - 1000;
-            }
-            else
-            {
-                gift = salary;
-            }
+                // output those values, nicely aligned
+                Console.WriteLine($"Age:             {age,12}" +
+                    $"\nSalary:          {salary,12:c}" +
+                    $"\nAdjusted Salary: {gift,12:c}");
 
-            // output those values, nicely aligned
-            Console.WriteLine($"Age: {age}" +
-                $"\nSalary: {salary}" +
-                $"\nAdjusted Salary: {gift}");
-            // then ask if they want to go again
+                // then ask if they want to go again
+                Console.Write("Would you like to play again? y/n: ");
+                userAnswer = Console.ReadLine().ToLower();
+
+            } while (userAnswer == "y" || userAnswer == "yes");
 
             Console.WriteLine("Thanks, bye!");
 
-            // debug
-            // add second question
-            // TODO scope: where should each variable be created?
         }
     }
 }
