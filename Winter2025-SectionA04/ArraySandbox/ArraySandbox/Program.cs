ac79d8f95d98eb060c0abed72fc9a2671ddda4e8
Dana <dmarsh@nait.ca>
2025-02-26 14:53:37 -0700

    Sneak peek at arrays

---
+++ Winter2025-SectionA04/ArraySandbox/ArraySandbox/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+ * Purpose: To learn about and apply methods related to arrays.
+ */
+namespace ArraySandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age = 0;
+
+            int[] ages = new int[22];
+            // this creates a new array that can hold 22 int values
+
+            double[] salaries = new double[22];
+
+            // alternatively, we can create an array AND give it starting values:
+            string[] candies = {"Kit Kat", "Reese's Peanut Butter Cups", "Aero"};
+
+            Console.WriteLine();
+        }
+    }
+}
