05f0b5145fe6c23833eba82cce0529750baddf29
Dana <dmarsh@nait.ca>
2025-02-28 11:03:52 -0700

    Add array examples

--- Winter2025-SectionA04/ArraySandbox/ArraySandbox/Program.cs
+++ Winter2025-SectionA04/ArraySandbox/ArraySandbox/Program.cs
@@ -18,6 +18,30 @@ namespace ArraySandbox
             string[] candies = {"Kit Kat", "Reese's Peanut Butter Cups", "Aero"};
 
             Console.WriteLine();
+
+            // physical size: the actual size of the array
+            // AKA how many elements it CAN contain
+            // AKA the length
+            // AKA the # inside the [] e.g. 
+            // int[] ages = new int[22];
+            // physical size = 22
+
+            // logical size: 
+            // how many of those compartments have actual values inside them
+            // e.g. inputted the age of 3 people
+            // logical size = 3
+
+
+
+            // BONUS CONTENT: arrays in arrays in arrays
+            int[] arrayA = new int[2];
+            int[] arrayB = new int[2];
+            int[] arrayC = new int[2];
+
+            int[][] middleArray = { arrayA, arrayB, arrayC };
+
+            int[][][] lastArray = { middleArray };
+
         }
     }
 }
