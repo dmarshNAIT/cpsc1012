ad96e044481cfcf2cb937da8278d7acb501cfda5
Dana <dmarsh@nait.ca>
2025-03-05 14:43:38 -0700

    Add example of parameter

--- Winter2025-SectionA04/SleepTrackerMethodDemo/Program.cs
+++ Winter2025-SectionA04/SleepTrackerMethodDemo/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Buffers.Text;
+﻿/*
+ * Purpose:  This program will allow a user to enter data into parallel arrays, access the values, edit the values, and read/write from/to a text file.
+ * Author:   Dana Marsh
+ * Modified: March 5, 2025
+ */
+
+using System.Buffers.Text;
 
 namespace SleepTrackerMethodDemo
 {
@@ -17,21 +23,56 @@ namespace SleepTrackerMethodDemo
             // Predict tonight's sleep
             //Formatting & colouring of text
 
+            // declare variables:
+            int sleepHoursThisWeek;
+            string name,
+                didYouSleep;
+
+            Console.WriteLine("Hello! Welcome to the Sleep Tracker!");
+
             // call the DisplayMainMenu():
             DisplayMainMenu();
+
+            // call GiveMeNumber, which RETURNs an int value
+            sleepHoursThisWeek = GiveMeNumber();
+            Console.WriteLine($"I slept {sleepHoursThisWeek} hours this week!");
+
+            // call our GetUserString method a few different times, with different questions
+            name = GetUserString("What is your name?");
+            didYouSleep = GetUserString("Did you sleep last night?");
+
+            Console.WriteLine("Thanks, goodbye!");
 
         }
 
         static void DisplayMainMenu()
         {
             Console.WriteLine("MENU OPTIONS:\n" +
-                "[e]nter values\n" +
-                "[v]iew values\n" +
-                "[s]ave to file\n" +
-                "[l]oad from file\n" +
-                "[p]redict tonight's sleep\n" +
-                "[q]uit\n");
+                "\t[e]nter values\n" +
+                "\t[v]iew values\n" +
+                "\t[s]ave to file\n" +
+                "\t[l]oad from file\n" +
+                "\t[p]redict tonight's sleep\n" +
+                "\t[q]uit\n");
         }
+
+        static int GiveMeNumber()
+        {
+            return 78;
+        }
+
+        // NEW CONTENT TO BE REVIEWED FRIDAY
+        static string GetUserString(string question)
+        {
+            string userResponse;
+            // ask the question
+            Console.Write(question);
+            // get the answer
+            userResponse = Console.ReadLine();
+            // return the answer
+            return userResponse;
+        }
+
 
     }
 }
