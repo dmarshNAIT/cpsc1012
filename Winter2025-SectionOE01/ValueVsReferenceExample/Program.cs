9786996ed94d96e9c263171c29984b74a3269c9b
Dana <dmarsh@nait.ca>
2025-03-13 19:37:53 -0600

    Add example of ref vs value types

---
+++ Winter2025-SectionOE01/ValueVsReferenceExample/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ValueVsReferenceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cmOfSnow = 10; // simple data types are passed by VALUE
+            ChangeNumber(cmOfSnow); // passing a copy of the value (10)
+            Console.WriteLine(cmOfSnow); // cmOfSnow is still 10
+
+            double[] temperatures = { -10, 0, -20 }; // arrays are REFERENCE type
+            ChangeElement(temperatures); // we're passing a reference to the array
+            Console.WriteLine(temperatures[0]); // now the element is 100
+        }
+
+        static void ChangeNumber(double number)
+        {
+            // this is a completely separate variable
+            number = 100;
+        }
+
+        static void ChangeElement(double[] numbers)
+        {
+            numbers[0] = 100;
+        }
+    }
+}
