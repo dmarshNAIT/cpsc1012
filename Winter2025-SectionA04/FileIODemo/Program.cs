092c0159c424c1e705afdfa9621e2e97224533aa
Dana <dmarsh@nait.ca>
2025-03-14 10:35:26 -0600

    Add demo of writing to a file

---
+++ Winter2025-SectionA04/FileIODemo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FileIODemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*** writing to a file ***/
+            try
+            {
+                // create our Writer object
+                StreamWriter writer = new StreamWriter("../../../TextyTestyDemoy.txt");
+                // instead of the default location, we are going up THREE levels in the folder structure. this means we are creating it in the same folder as Program.cs
+                // we are using a RELATIVE path (rather than an ABSOLUTE path)
+
+                // write to the file
+                writer.Write("I JUST WANNA WATCH THE CODE VERSION OF SUFFERING");
+                writer.WriteLine("!!!!");
+
+                // close the stream
+                writer.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong saving the file.");
+            }
+
+            /*** reading FROM a file ***/
+        }
+    }
+}
