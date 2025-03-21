765c1351eebf828a98b81d09adcd947420678a25
Dana <dmarsh@nait.ca>
2025-03-21 11:48:19 -0600

    Add example of drawing a box 📦

---
+++ Winter2025-SectionA04/DrawingDemo/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace DrawingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawBox();
+
+            Console.WriteLine("now v2:");
+            DrawBox(10, 6);
+
+            Console.WriteLine("And now a tiny box:");
+            DrawBox(4, 3);
+
+            Console.WriteLine("Tiny tiny box:");
+            DrawBox(1, 1);
+        }
+
+        static void DrawBox()
+        {
+            // print the first line:
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+
+            // print the middle lines:
+            // for each line
+            // if it's the first character OR the last character, print *
+            // otherwise, print a space
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 10; column++)
+                {
+                    if (column == 0 || column == 9)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // print the last line:
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+
+        static void DrawBox(int width, int height) // overloaded method!
+        {
+            if (width < 2 || height < 2) // defensive coding
+            {
+                Console.WriteLine("Sorry, that's too tiny.");
+            }
+            else
+            {
+                // print the first line:
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+
+                // print the middle lines:
+                // for each line
+                // if it's the first character OR the last character, print *
+                // otherwise, print a space
+                for (int row = 0; row < (height - 2); row++)
+                {
+                    for (int column = 0; column < width; column++)
+                    {
+                        // if it's the first or last column, print a *:
+                        if (column == 0 || column == (width - 1))
+                        {
+                            Console.Write('*');
+                        }
+                        // otherwise, print a space
+                        else
+                        {
+                            Console.Write(' ');
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                // print the last line:
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Winter2025-SectionA04/LoopingSentinelValue/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Demo of using sentinel-controlled loops.
+ * 
+ * Mar 21, 2025
+ */ 
+namespace LoopingSentinelValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an array:
+            int[] calories = new int[7];
+            int logicalSize = 0;
+            bool userWantsToContinue = true;
+
+            // loop through the array and add values:
+            // start at index 0
+            // go until the end of the array OR until the user asks to quit
+            // ask the user to enter a value
+            // if they enter a "real" value: save it and update logicalSize
+            // if they enter a "bad" value: complain
+            // if they enter our sentinel value: we want to exit the loop
+            for (logicalSize = 0 ; logicalSize < calories.Length && userWantsToContinue; )
+            {
+                int enteredValue = GetUserInt("Please enter the # of calories consumed or enter -9 to exit: ");
+                // validate input before going any further:
+                while (enteredValue < 0 && enteredValue != -9)
+                {
+                    // bad input!!
+                    Console.WriteLine("Please enter a valid value.");
+                    enteredValue = GetUserInt("Please enter the # of calories consumed or enter -9 to exit: ");
+                }
+
+                // by this point, we know we have a valid number, so we can branch accordingly
+                if (enteredValue == -9)
+                {
+                    userWantsToContinue = false;
+                }
+                else
+                {
+                    calories[logicalSize] = enteredValue; // add the value into the current element of the array
+                    logicalSize++;
+                }
+            }
+         
+
+            Console.WriteLine("Bye!");
+
+        }
+
+        /// <summary>
+        /// A reusable method which prompts the user for input then returns their inputted int.
+        /// </summary>
+        /// <param name="question">A message to display to the user.</param>
+        /// <returns>A user-entered int.</returns>
+        static int GetUserInt(string question)
+        {
+            while (true)
+            {
+                // ask the user a question
+                Console.Write(question);
+
+                // read in their answer
+                // try to parse it as a int
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                    // if that works, return it
+                    // this is the only way to exit the method
+                }
+                catch  // otherwise, loop back & try again
+                {
+                    Console.WriteLine("Please enter a valid number. Try again.");
+                }
+            } // end of loop
+        } // end of method
+    }
+}
