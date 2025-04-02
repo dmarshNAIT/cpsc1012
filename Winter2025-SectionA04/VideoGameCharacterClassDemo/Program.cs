651c2ea47d297e3dbbce4147b9ff0c8e3d2c885c
Dana <dmarsh@nait.ca>
2025-04-02 14:49:51 -0600

    Add first draft of VideoGameCharacter

---
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoGameCharacterClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an object, using the default values
+
+            VideoGameCharacter kirby = new VideoGameCharacter();
+        }
+    }
+}
---
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
@@ -0,0 +1,27 @@
+﻿namespace VideoGameCharacterClassDemo
+{
+    internal class VideoGameCharacter
+    {
+
+        /*** CHARACTERISTICS ***/
+        private string _name = "Unknown";
+        private int _hp = 100;
+        private string _armor = "None";
+        private string _weapon = "Fist";
+
+        // stamina
+        // gender
+        // armor/weapon
+        // hunger
+        // luck
+        // mana
+        // evasiveness
+
+
+        /*** BEHAVIOUR ***/
+        // attack?
+        // dance?
+
+
+    }
+}
