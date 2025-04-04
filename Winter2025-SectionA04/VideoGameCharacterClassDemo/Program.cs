8eae9def20a7e08552bf5258bb878faf4333adfb
Dana <dmarsh@nait.ca>
2025-04-04 11:20:54 -0600

    Add mutator

--- Winter2025-SectionA04/VideoGameCharacterClassDemo/Program.cs
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/Program.cs
@@ -9,6 +9,17 @@
             VideoGameCharacter kirby = new VideoGameCharacter();
             // we can create a character using our parameterized constructor
             VideoGameCharacter zelda = new VideoGameCharacter("Zelda", 100, "Light", "Fist");
+
+            // test our accessor methods:
+            Console.WriteLine($"The HP of {zelda.GetName()} is {zelda.GetHP()}.");
+            Console.WriteLine($"The Armor of {zelda.GetName()} is {zelda.GetArmor()}.");
+            Console.WriteLine($"The Weapon of {zelda.GetName()} is {zelda.GetWeapon()}.");
+
+            // test our mutator:
+            Console.WriteLine($"The object's name is {kirby.GetName()}");
+            kirby.SetName("Kirby");
+            Console.WriteLine($"The object's name is {kirby.GetName()}");
+
         }
     }
 }
--- Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
@@ -21,17 +21,35 @@
         /*** BEHAVIOUR ***/
         /*** CONSTRUCTORS ***/
         // "greedy" constructor (takes ALL the parameters it can)
-        public VideoGameCharacter (string name, int hp, string armor, string weapon)
+        public VideoGameCharacter(string name, int hp, string armor, string weapon)
         {
             // TODO: add validation
             _name = name;
-            _hp = hp;   
+            _hp = hp;
             _armor = armor;
             _weapon = weapon;
         }
 
         // no-argument constructor (our version of the default constructor)
         public VideoGameCharacter() { }
+
+        /*** ACCESSOR METHODS ***/
+        public string GetName() { return _name; }
+        public int GetHP() { return _hp; }
+        public string GetArmor() { return _armor; }
+        public string GetWeapon() { return _weapon; }
+
+        /*** MUTATOR METHODS ***/
+        // name: name must be 2-10 characters long
+        public void SetName(string name) { 
+            _name = name;
+            // TODO: MISSING VALIDATION
+        }
+
+
+        // hp: this cannot be directly set to a new value
+        // TODO: armor: this can change but it must be picked from a list of things they have
+        // TODO: weapon: same idea as armor
 
 
 
