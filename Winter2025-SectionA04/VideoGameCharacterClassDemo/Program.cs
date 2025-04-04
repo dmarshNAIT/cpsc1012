c7bb8ede26eb66d9e6b70517a3d5fe7b4a847abc
Dana <dmarsh@nait.ca>
2025-04-04 10:47:05 -0600

    Add constructors

--- Winter2025-SectionA04/VideoGameCharacterClassDemo/Program.cs
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/Program.cs
@@ -7,6 +7,8 @@
             // instantiate an object, using the default values
             // the name of the object is "kirby"
             VideoGameCharacter kirby = new VideoGameCharacter();
+            // we can create a character using our parameterized constructor
+            VideoGameCharacter zelda = new VideoGameCharacter("Zelda", 100, "Light", "Fist");
         }
     }
 }
--- Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
@@ -19,6 +19,22 @@
 
 
         /*** BEHAVIOUR ***/
+        /*** CONSTRUCTORS ***/
+        // "greedy" constructor (takes ALL the parameters it can)
+        public VideoGameCharacter (string name, int hp, string armor, string weapon)
+        {
+            // TODO: add validation
+            _name = name;
+            _hp = hp;   
+            _armor = armor;
+            _weapon = weapon;
+        }
+
+        // no-argument constructor (our version of the default constructor)
+        public VideoGameCharacter() { }
+
+
+
         // attack?
         // dance?
 
