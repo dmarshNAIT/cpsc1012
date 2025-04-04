8e51916ffa72092761efaa44ac2527bdcb3fd524
Dana <dmarsh@nait.ca>
2025-04-03 20:48:27 -0600

    Add constructor & accessor methods

--- Winter2025-SectionOE01/VideoGameClassDemo/Program.cs
+++ Winter2025-SectionOE01/VideoGameClassDemo/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             VideoGameCharacter letmesoloher = new VideoGameCharacter();
+            // ^ this line will only work if we have a default constructor
+            // (which means we didn't create ANY constructors so the compiler made one automatically), or
+            // if we create a no-argument constructor
+
+            VideoGameCharacter mario = new VideoGameCharacter("Mario", 1, 85);
+
+            // let's test our accessor methods:
+            Console.WriteLine($"The name of the character is {mario.GetName()}.");
+            Console.WriteLine($"The level of the character is {mario.GetLevel()}.");
+            Console.WriteLine($"The HP of the character is {mario.GetHP()}.");
+
+
         }
     }
 }
--- Winter2025-SectionOE01/VideoGameClassDemo/VideoGameCharacter.cs
+++ Winter2025-SectionOE01/VideoGameClassDemo/VideoGameCharacter.cs
@@ -4,14 +4,46 @@
     {
 
         /**** CHARACTERISTICS ****/
-        // level, hp, referenceID, characterID, name, special powers, mana/resource, aggression, morality, speed, combat style
+        // referenceID, characterID, special powers, mana/resource, aggression, morality, speed, combat style, attack modifier
+        // TODO: add a static field
         private string _name = "Unknown";
         private float _level = 1;
-        private double _hp = 100; 
+        private double _hp = 100;
 
 
         /**** BEHAVIOUR ****/
+        /*** CONSTRUCTORS ****/
+
+        // parameterized constructor
+        /// <summary>
+        /// Construct a new VideoGameCharacter object
+        /// </summary>
+        /// <param name="name">The character's name</param>
+        /// <param name="level">The level, defaults to 1</param>
+        /// <param name="hp">The HP, defaults to 100</param>
+        public VideoGameCharacter(string name, float level, double hp) {
+            _name = name;
+            _level = level;
+            _hp = hp;
+        }
+
+        // no-argument constructor:
+        /// <summary>
+        /// Construct a new VideoGameCharacter object
+        /// </summary>
+        public VideoGameCharacter() { }
+
+        /**** ACCESSOR METHODS ****/
+        public string GetName() { return _name; }
+
+        public float GetLevel() { return _level; }
+
+        public double GetHP() {  return _hp; }
+
+
+
         // LevelUp, Heal, TakeDamage, IncreaseStat e.g. speed
         // stretch: DoDamage
+        // perhaps adding something that interacts with another class?
     }
 }
