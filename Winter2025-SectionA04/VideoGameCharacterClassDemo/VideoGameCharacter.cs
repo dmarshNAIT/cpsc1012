084d96c07761b30d8d94635bb18ed660c1f27d30
Dana Marsh <dmarsh@nait.ca>
2025-04-08 14:47:25 -0600

    Add properties & armor functionality

--- Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
+++ Winter2025-SectionA04/VideoGameCharacterClassDemo/VideoGameCharacter.cs
@@ -1,4 +1,6 @@
-﻿namespace VideoGameCharacterClassDemo
+﻿using System.Xml.Linq;
+
+namespace VideoGameCharacterClassDemo
 {
     internal class VideoGameCharacter
     {
@@ -8,6 +10,10 @@
         private int _hp = 100;
         private string _armor = "None";
         private string _weapon = "Fist";
+        private List<string> _armorInventory = new List<string>();
+        // TODO: add armor to the list
+        // TODO: view armor that we have
+        // TODO: FUTURE challenge: perhaps instead of a list of strings, we have a list of Armor objects, having properties of "type of armor", "strength", "what it protects against..."
 
         // stamina
         // gender
@@ -17,17 +23,35 @@
         // mana
         // evasiveness
 
+        /*** PROPERTIES ***/
+        public string Armor
+        {
+            get { return _armor; }
+            set
+            {
+                // we are only allowed to set Armor to a value that is ALREADY in the _armorInventory list
+                if (_armorInventory.Contains(value))
+                {
+                    _armor = value;
+                } else // not a valid option
+                {
+                    throw new Exception("NOOOO. You must select something from your inventory. :)");
+                }
+            }
+        }
+
 
         /*** BEHAVIOUR ***/
         /*** CONSTRUCTORS ***/
         // "greedy" constructor (takes ALL the parameters it can)
         public VideoGameCharacter(string name, int hp, string armor, string weapon)
         {
-            // TODO: add validation
-            _name = name;
-            _hp = hp;
-            _armor = armor;
-            _weapon = weapon;
+            
+            Name = name; // take advantage of the validation in your setter or property
+
+            _hp = hp; // TODO: add validation
+            _armor = armor; // TODO: add validation
+            _weapon = weapon; // TODO: add validation
         }
 
         // no-argument constructor (our version of the default constructor)
@@ -53,7 +77,22 @@
             }
         }
 
-        // TODO on Tuesday: create the same thing but as a property
+        // instead of creating GetName() and SetName(), another approach is to create a single property called Name
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Trim().Length >= 2 && value.Trim().Length <= 10)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("The name must be 2-10 characters long.");
+                }
+            }
+        }
 
 
         // hp: this cannot be directly set to a new value
@@ -64,7 +103,113 @@
 
         // attack?
         // dance?
+        // signature attack?
 
+        /*** OTHER METHODS ***/
+        public void LootBattlefield()
+        {
+            // ask the user what they looted
+            string loot = GetUserString("What did you get from the loot box? ");
+            // add the item to the list
+            _armorInventory.Add(loot);
+        }
+
+        /// <summary>
+        /// A reusable method which prompts the user for input then returns their inputted string.
+        /// </summary>
+        /// <param name="question">A message to display to the user.</param>
+        /// <returns>The user-inputted text.</returns>
+        public static string GetUserString(string question)
+        {
+            string userResponse;
+            // ask the question
+            Console.Write(question);
+            // get the answer
+            userResponse = Console.ReadLine();
+            // return the answer
+            return userResponse;
+        }
+
+        /// <summary>
+        /// A reusable method which prompts the user for input then returns their inputted double.
+        /// </summary>
+        /// <param name="question">A message to display to the user.</param>
+        /// <returns>A user-entered double.</returns>
+        public static double GetUserDouble(string question)
+        {
+            while (true)
+            {
+                // ask the user a question
+                Console.Write(question);
+
+                // read in their answer
+                // try to parse it as a double
+                try
+                {
+                    return double.Parse(Console.ReadLine());
+                    // if that works, return it
+                    // this is the only way to exit the method
+                }
+                catch  // otherwise, loop back & try again
+                {
+                    Console.WriteLine("Please enter a valid number. Try again.");
+                }
+            } // end of loop
+        } // end of method
+
+        /// <summary>
+        /// A reusable method which prompts the user for input then returns their inputted int.
+        /// </summary>
+        /// <param name="question">A message to display to the user.</param>
+        /// <returns>A user-entered int.</returns>
+        public static int GetUserInt(string question)
+        {
+            while (true)
+            {
+                // ask the user a question
+                Console.Write(question);
+
+                // read in their answer
+                // try to parse it as a int
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                    // if that works, return it
+                    // this is the only way to exit the method
+                }
+                catch  // otherwise, loop back & try again
+                {
+                    Console.WriteLine("Please enter a valid number. Try again.");
+                }
+            } // end of loop
+        } // end of method
+
+        /// <summary>
+        /// A reusable method which prompts the user for input then returns their inputted char.
+        /// </summary>
+        /// <param name="question">A message to display to the user.</param>
+        /// <returns>A user-entered char.</returns>
+        public static char GetUserChar(string question)
+        {
+            while (true)
+            {
+                // ask the user a question
+                Console.Write(question);
+
+                // read in their answer
+                // try to parse it as a char
+                try
+                {
+                    return char.Parse(Console.ReadLine());
+                    // if that works, return it
+                    // this is the only way to exit the method
+                }
+                catch  // otherwise, loop back & try again
+                {
+                    Console.WriteLine("Please enter a valid character. Try again.");
+                }
+            } // end of loop
+        } // end of method
 
     }
 }
