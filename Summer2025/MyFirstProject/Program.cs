6b1e34642f1b28ba53e4017ec08876b9d1b991d6
Dana <dmarsh@nait.ca>
2025-05-08 21:51:38 -0600

    Create our first project! 🥳

---
+++ Summer2025/MyFirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("it only gets easier from here!");
+        }
+    }
+}
