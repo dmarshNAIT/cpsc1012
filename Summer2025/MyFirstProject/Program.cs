3552df072b507b195b63f4e518744d158aca1511
Dana <dmarsh@nait.ca>
2025-05-13 21:45:56 -0600

    Add examples of variables

--- Summer2025/MyFirstProject/Program.cs
+++ Summer2025/MyFirstProject/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
+            // this is a comment! it is for humans.
+            // the next statement prints a message to the screen.
+
+            /* this is also a comment
+              I'm still in the comment
+              the comment can go on
+              and on
+              and on
+             */
+
             Console.WriteLine("it only gets easier from here!");
+            Console.WriteLine("message 2");
+            // Console.WriteLine prints a message then goes to the next line
+            Console.Write("message 3");
+            Console.Write("message 4");
+            // Console.Write prints a message and stays on the same line
+            // this is what we want to use for user input prompts
+
+            Console.WriteLine("\nToday\n is\n \t\tTuesday\n.");
+            // \n creates a new line
+            // \t is "tab": this indents the output
+
+            // create a variable to keep track of the number of games won
+            int gamesWon;
+            // we can assign a value immediately, or later on.
+            // variables are named starting with a lowercase letter
+            gamesWon = 3;
+
+            double averageGoalsPerGame;
         }
     }
 }
---
+++ Summer2025/TopLevelStatement/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// this is essentially hiding the lines that start and end the namespace, the class, and the method
+// instead, we just see the CONTENTS of the Main
