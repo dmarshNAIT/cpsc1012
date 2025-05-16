04024b8df0ff757f4114c65af1966f85f93d9543
Dana <dmarsh@nait.ca>
2025-05-15 21:41:55 -0600

    Add our average winrate calculator h🏒

--- Summer2025/MyFirstProject/Program.cs
+++ Summer2025/MyFirstProject/Program.cs
@@ -27,12 +27,69 @@
             // \t is "tab": this indents the output
 
             // create a variable to keep track of the number of games won
-            int gamesWon;
+            int cookiesEaten;
             // we can assign a value immediately, or later on.
             // variables are named starting with a lowercase letter
-            gamesWon = 3;
+            cookiesEaten = 3;
 
             double averageGoalsPerGame;
+
+            // May 15 class example:
+            // let's do a simple calculation
+
+            // data going in:
+            // how many games they won
+            // how many games they lost
+
+            // data coming out:
+            // what percentage of the games they've won?
+
+            // e.g. won 4, lost 1: they've won 80% of the games
+
+            // SEQUENCE:
+            // declare variables
+            // ask the user to input games won
+            // ask the user to input games lost
+            // calculate total # of games
+            // calculate their win rate
+            // display results as a percent (not decimal)
+
+            /************* CODING TIME ************/
+            // declare & assign variables
+            int gamesWon = 4,
+                gamesLost = 1,
+                totalGames;
+            double winRate;
+
+            // calculate total # of games
+            totalGames = gamesLost + gamesWon;
+
+            // calculate their win rate
+            winRate = gamesWon / totalGames;
+            // this line is doing integer division: 4/5 --> 0
+            winRate = gamesWon * 1.0 / totalGames;
+            // fix #1: incorporate a double into the equation
+            winRate = (double)gamesWon / totalGames;
+            // or fix #2: cast as a double
+
+            // display results as a percent (not decimal)
+            Console.WriteLine("The average win rate is " + winRate * 100 + "%");
+
+            // let's play around with the cast operator
+
+            // let's try casting an int to a double
+            double myDouble = (double)33;
+            Console.WriteLine("myDouble is " + myDouble);
+
+            // let's try casting a double to an int
+            int myInt = (int)21.7;
+            Console.WriteLine("myInt is " + myInt);
+            // ints cannot store decimals, so those values are lost
+
+            // weird things can happen when we cast to a "smaller" data type: be careful!!!
+            int bigNumber = 123456789;
+            byte test = (byte)bigNumber;
+            Console.WriteLine(test); // test is 21
         }
     }
 }
