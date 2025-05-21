ac9b13bddf8de7135c35a812049d908361bd385f
Dana <dmarsh@nait.ca>
2025-05-20 20:58:28 -0600

    Add user input demo ❓

---
+++ Summer2025/UserInputDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace UserInputDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create variables
+            string username,
+                userInput;
+
+            int userAge;
+
+            // ask a question
+            Console.Write("What is your name? ");
+
+            // read in their answer, AND SAVE IT
+            username = Console.ReadLine();
+
+            // print out a welcome
+            // v1: string concatenation
+            Console.WriteLine("Welcome, " + username);
+            // v1: string interpolation
+            Console.WriteLine($"Welcome, {username}");
+
+            // ask another question
+            Console.Write("How old are you? ");
+
+            // save the answer
+            userInput = Console.ReadLine();
+            userAge = int.Parse(userInput);
+
+            // or, combined:
+            // userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You are {userAge} years old.");
+
+        }
+    }
+}
