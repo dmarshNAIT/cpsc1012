b8a03c52b457f1946950d5e7408f20eb2e081ed4
Dana <dmarsh@nait.ca>
2025-05-20 20:58:16 -0600

    Add v1 of Simple Calculator 🧮

---
+++ Summer2025/SimpleCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputOne,
+                inputTwo,
+                sum,         // result of addition
+                difference,  // result of subtraction
+                product;     // result of multiplication
+            double quotient; // result of division
+            string userInput;
+
+            //- get & save 1st user input
+            Console.Write("Please enter 1st number: ");
+            inputOne = int.Parse(Console.ReadLine());
+
+            //- get & save 2nd user input
+            Console.Write("Please enter 2nd number: ");
+            userInput = Console.ReadLine();
+            inputTwo = int.Parse(userInput);
+
+            //- calculate results
+            sum = inputOne + inputTwo;
+            difference = inputOne - inputTwo;
+            product = inputOne * inputTwo;
+            quotient = inputOne / inputTwo;
+
+            //- display results
+            Console.WriteLine($"The sum is {sum}.\n" +
+                $"The difference is {difference}.\n" +
+                $"The product is {product}.\n" +
+                $"The quotient is {quotient}.\n");
+  
+        }
+    }
+}
