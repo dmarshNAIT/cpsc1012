5d9ec386fa63ff0b1b2adb42c367e7ab03a1d97e
Dana <dmarsh@nait.ca>
2025-07-03 19:35:58 -0600

    Add pseudocode plan 🥕

---
+++ Summer2025/ArrayPractice-FarmersMarket/Program.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * This program stores the prices of produce items sold at a market.
+ * 
+ * 0
+ */ 
+using static System.Formats.Asn1.AsnWriter;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
+
+namespace ArrayPractice_FarmersMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //One array contains the names of the produce e.g.zucchini
+            //Another array contains the price per item in CAD
+
+            // welcome
+
+            // display menu
+            // handle user's choice
+            // if they didn't choose to quit, keep looping
+
+            // goodbye message
+
+        }
+
+        // TODO: method to Display Menu
+        // TODO: method to handle user's choice
+        // TODO: method to View Items
+        // TODO: method to Add Items
+        // TODO: method to Edit Items
+        // TODO: method to Delete Items
+        // TODO: (FUTURE) ability to read and write from a file
+    }
+}
+
+            //Future extensions:
+            //Quantity in stock?
+            //Weight / diameter ?
