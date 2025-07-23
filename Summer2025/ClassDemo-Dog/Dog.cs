54dac4acd299a55e5ce3701f726dd019e073debb
Dana <dmarsh@nait.ca>
2025-07-22 20:46:28 -0600

    Add Dog example 🐶

---
+++ Summer2025/ClassDemo-Dog/Dog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo_Dog
+{
+    internal class Dog
+    {
+        /**** characteristics ****/
+        private string _name = "Unknown"; // initializing the member variable
+        // microchip ID
+        // breed
+        // hair length
+        // shed vs non-shed
+        // tail
+        // allergenic
+        private string _size = "small"; // initializing the member variable
+        // attitude
+        // colour
+        private int _age; // the default value for an int is 0
+        // gender/sex
+        // neuter/spay
+        // indoor/outdoor
+        // how smart they are
+
+        /*** CONSTRUCTORS ***/
+        // no-argument constructor
+        public Dog() { }
+
+        // parameterized constructor
+        public Dog(string name, string size, int age)
+        {
+            _name = name;
+            _size = size;
+            _age = age;
+        }
+
+        public Dog(string name, string size)
+        {
+            _name = name;
+            _size = size;
+        }
+
+
+    }
+}
---
+++ Summer2025/ClassDemo-Dog/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassDemo_Dog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog fido = new Dog(); // instantiate an object using no-arg constructor
+            Dog moose = new Dog("Moose", "large", 4);
+            Dog puppy = new Dog("Spot", "small");
+        }
+    }
+}
