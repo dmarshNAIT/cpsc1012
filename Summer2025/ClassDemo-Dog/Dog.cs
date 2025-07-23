0907a5e56e62e87bfcffdce39e2deb4ff723d934
Dana <dmarsh@nait.ca>
2025-07-22 21:47:10 -0600

    Add property validation

--- Summer2025/ClassDemo-Dog/Dog.cs
+++ Summer2025/ClassDemo-Dog/Dog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ClassDemo_Dog
 {
@@ -72,6 +73,18 @@ namespace ClassDemo_Dog
         public string Name
         {
             get { return _name; }
+            set {
+                if (value.Trim().Length <= 0 || value.Trim().Length > 21)
+                {
+                    throw new Exception("Dog name must be between 1 & 21 characters in length.");
+                    // alternatively, we could have created an ArgumentException
+                }
+                else
+                {
+                    _name = value.Trim();
+                }
+            }
+
         }
 
     }
