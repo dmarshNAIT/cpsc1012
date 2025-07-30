9ff04f44fb46deb2e125587fa0d93f0202bdf501
Dana <dmarsh@nait.ca>
2025-07-29 20:36:27 -0600

    Create BetterItem fields & properties

---
+++ Summer2025/ClassAndListPractice-DogStore/BetterItem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndListPractice_DogStore
+{
+    internal class BetterItem
+    {
+        // instance fields & properties
+        private string _name;
+        private double _price;
+        private int _quantity;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                // if the name is empty, throw Exception
+                // if the name is NOT empty, use the name
+                if (value.Trim().Length > 0)
+                    _name = value.Trim();
+                else
+                    throw new Exception("Name cannot be empty.");
+
+            }
+        }
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                // if price > 0, use it. otherwise, throw Exception.
+                if (value > 0)
+                    _price = value;
+                else
+                    throw new Exception("Price must be greater than 0.");
+            }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                // if quantityice > 0, use it. otherwise, throw Exception.
+                if (value > 0)
+                    _quantity = value;
+                else
+                    throw new Exception("Quantity must be greater than 0.");
+            }
+        }
+
+        // constructors
+
+
+        // public methods
+
+
+        // private methods
+
+
+
+        /**
+            fields:
+
+
+            Quantity (must be >0)
+            Discount (0-100)
+            Constructors
+            Read-only properties: 
+            E.g. Dog treats, $2 each, quantity = 5, discount = 25%
+            TotalCostPerItem = 2 * 5 - discount = 7.50
+            DiscountLevel (none, Sale, Clearance)
+         */
+    }
+}
---
+++ Summer2025/ClassAndListPractice-DogStore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassAndListPractice_DogStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
