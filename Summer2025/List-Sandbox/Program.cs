3dd010e047ef62f9941ac6c825a45f86df0ebef2
Dana <dmarsh@nait.ca>
2025-07-29 20:36:04 -0600

    Quick demo of Lists 🍿

---
+++ Summer2025/List-Sandbox/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace List_Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> movies = new List<string>();
+
+            movies.Add("Zoolander");
+            movies.Add("Lucky Number Slevin");
+            movies.Add("Frozen");
+            movies.Add("Zoolander II");
+            movies.Add("Oppenheimer");
+            movies[4] = "Barbie";
+            movies.Capacity = 5;
+        }
+    }
+}
